b06411371006f91b469a81e0dc85aefcc3e8e60a
Alexis Sukrieh <sukria@sukria.net>
2011-08-15 02:24:49 +0200

    first Dancer::Core::Route class with tests

--- lib/Dancer/Core/Route.pm
+++ lib/Dancer/Core/Route.pm
@@ -1,18 +1,18 @@
 package Dancer::Core::Route;
+# ABSTRACT: class for Dancer route handler objects
 
 use strict;
 use warnings;
 
 use Moo;
 use Dancer::Moo::Types;
+use Carp 'croak';
 
-use Carp;
+=attr method
 
-use Dancer::App;
-use Dancer::Logger;
-use Dancer::Config 'setting';
-use Dancer::Request;
-use Dancer::Response;
+the HTTP method of the route (lowercase). Required.
+
+=cut
 
 has method => (
     is => 'ro',
@@ -20,31 +20,49 @@ has method => (
     required => 1,
 );
 
-1;
-__END__
+=attr code
+
+The code reference to execute when the route is ran. Required.
+
+=cut
+
+has code => (
+    is => 'ro',
+    required => 1,
+    isa => sub { Dancer::Moo::Types::CodeRef(@_) },
+);
+
+=attr regexp
+
+The regular expression that defines the path of the route.
+Required. Coerce from Dancer's route I<patterns>.
+
+=cut
+
+has regexp => (
+    is => 'rw',
+    required => 1,
+);
+
+=attr prefix
+
+The prefix to prepend to the C<regexp>. Optional.
+
+=cut
 
 has prefix => (
     is => 'ro',
     isa => sub { Dancer::Moo::Types::DancerPrefix(@_) },
 );
 
-has code => (
-    is => 'ro',
-    isa => sub { Dancer::Moo::Types::CodeRef(@_) },
-);
+=attr options 
 
-has prev => (
-    is => 'rw',
-    isa => sub { Dancer::Moo::Types::ObjectOf( 'Dancer::Route' => @_) },
-);
+A HashRef of conditions on which the matching will depend. Optional.
 
-has next => (
-    is => 'rw',
-    isa => sub { Dancer::Moo::Types::ObjectOf( 'Dancer::Route' => @_) },
-);
+=cut
 
 has options => (
-    is => 'rw',
+    is => 'ro',
     isa => sub { Dancer::Moo::Types::HashRef( @_) },
     trigger => \&_check_options,
 );
@@ -52,7 +70,8 @@ has options => (
 sub _check_options {
     my ($self, $options) = @_;
 
-    my @_supported_options = Dancer::Request->get_attributes();
+#  TODO  my @_supported_options = Dancer::Request->get_attributes();
+    my @_supported_options;
     my %_options_aliases = (agent => 'user_agent');
 
     return 1 unless defined $options;
@@ -65,72 +84,160 @@ sub _check_options {
     return 1;
 }
 
-has should_capture => (
+# private attributes
+
+has _should_capture => (
     is => 'rw',
     isa => sub { Dancer::Moo::Types::Bool(@_) },
 );
 
-has match_data => (
+has _match_data => (
     is => 'rw',
     isa => sub { Dancer::Moo::Types::HashRef( @_) },
+    trigger => sub {
+        my ($self, $value) = @_;
+        # TODO $request->_set_route_params($match_data);
+    },
 );
 
-has params => (
+has _params => (
     is => 'rw',
     isa => sub { Dancer::Moo::Types::ArrayRef(@_) },
     default => sub { [] },
 );
 
-# backward compat
-has pattern => (
-    is => 'ro',
-    trigger => sub { $_[0]->regexp($_[1] ) },
-);
+=method match
 
-has regexp => (
-    is => 'ro',
-    required => 1,
-    
-    # we may have non-regexp value here, Dancer route patterns
-    # then, we must coerce them into plain Regexp
-    trigger => sub {  
-        my ($self, $value) = @_;
+Try to match the route with a given pair of method/path.
+Returns the hash of matching data if success (captures and values of the route
+against the path) or undef if not.
 
-        # already a Regexp, so capture is true
-        if (ref($value) eq 'Regexp') {
-            my $regexp = $self->regexp;
-            $self->regexp(qr{^$regexp$});
-            $self->should_capture(1);
-            return $value;
+    my $match = $route->match( get => '/hello/sukria' );
+
+=cut
+
+sub match {
+    my ($self, $method, $path) = @_;
+
+    my %params;
+    my @values = $path =~ $self->regexp;
+
+    # the regex comments are how we know if we captured
+    # a splat or a megasplat
+    if( my @splat_or_megasplat
+            = $self->regexp =~ /\(\?#((?:mega)?splat)\)/g ) {
+        for ( @values ) {
+            $_ = [ split '/' => $_ ] if ( shift @splat_or_megasplat ) =~ /megasplat/;
         }
+    }
 
-        my ($compiled, $params, $should_capture) = 
-          @{ _build_regexp_from_string(@_) };
+    # if some named captures found, return captures
+    # no warnings is for perl < 5.10
+    if (my %captures =
+        do { no warnings; %+ }
+      )
+    {
+        return $self->_match_data({captures => \%captures});
+    }
 
-        $self->should_capture(defined $should_capture ? $should_capture : 0 );
-        $self->params($params || []);
-        $self->{regexp} = $compiled;
+    return unless @values;
 
-        return $compiled;
-    },
-);
+    # save the route pattern that matched
+    # TODO : as soon as we have proper Dancer::Internal, we should remove
+    # that, it's just a quick hack for plugins to access the matching
+    # pattern.
+    # NOTE: YOU SHOULD NOT USE THAT, OR IF YOU DO, YOU MUST KNOW
+    # IT WILL MOVE VERY SOON
+    # $request->{_route_pattern} = $self->regexp;
+
+    # named tokens
+    my @tokens = @{$self->_params};
+
+    if (@tokens) {
+        for (my $i = 0; $i < @tokens; $i++) {
+            $params{$tokens[$i]} = $values[$i];
+        }
+        return $self->_match_data(\%params);
+    }
+
+    elsif ($self->_should_capture) {
+        return $self->_match_data({splat => \@values});
+    }
+
+    return $self->_match_data({});
+}
+
+=method execute
+
+Runs the coderef of the route
+
+=cut
+
+sub execute {
+    my ($self) = @_;
+    return $self->code->();
+}
+
+# private subs
 
 sub BUILD {
     my ($self) = @_;
 
     # prepend the prefix to the regexp if any
-    if (defined $self->prefix) {
-        my $prefix = $self->prefix;
-        my $regexp = $self->regexp;
+    # this is done in BUILD instead of a trigger in order to be sure that the regexp
+    # attribute is set when this is ran.
+    $self->_init_prefix if defined $self->prefix;
 
-        if ($regexp !~ /^$prefix/) {
-            $self->regexp(qr{${prefix}${regexp}});
-        }
+    # now we can build the regexp
+    $self->_init_regexp;
+}
+
+# alter the regexp according to the prefix set, if any.
+sub _init_prefix {
+    my ($self) = @_;
+    
+    my $prefix = $self->prefix;
+    my $regexp = $self->regexp;
+    return unless defined $prefix;
+
+    if (ref($regexp) eq 'Regexp') {
+        return $self->regexp(qr{${prefix}${regexp}}) 
+          if $regexp !~ /^$prefix/;
+        return;
     }
 
-    $self->set_previous($self->prev) if $self->prev;
+    if ($self->regexp eq '/') {
 
-    return $self;
+        # if pattern is '/', we should match:
+        # - /prefix/
+        # - /prefix
+        # this is done by creating a regex for this case
+        my $qpattern = quotemeta( $self->regexp);
+        my $qprefix  = quotemeta( $self->prefix );
+        my $new_regxp = qr/^$qprefix(?:$qpattern)?$/;
+
+        return $self->regexp( $new_regxp );
+    }
+
+    return $self->regexp($prefix . $self->regexp);
+}
+
+sub _init_regexp {
+    my ($self) = @_;
+    my $value = $self->regexp;
+
+    # already a Regexp, so capture is true
+    if (ref($value) eq 'Regexp') {
+        $self->_should_capture(1);
+        return $value;
+    }
+
+    my ($compiled, $params, $should_capture) =
+      @{_build_regexp_from_string($value)};
+
+    $self->_should_capture(defined $should_capture ? $should_capture : 0);
+    $self->_params($params || []);
+    $self->regexp( $compiled );
 }
 
 sub _build_regexp_from_string {
@@ -162,99 +269,18 @@ sub _build_regexp_from_string {
     # escape slashes
     $string =~ s/\//\\\//g;
 
-    return ["^${string}\$", \@params, $capture];
+    return ["^$string\$", \@params, $capture];
 }
 
-sub set_previous {
-    my ($self, $prev) = @_;
+1;
+__END__
 
-    $self->prev($prev);
-    $self->prev->next( $self );
-
-    return $prev;
-}
-
-sub save_match_data {
-    my ($self, $request, $match_data) = @_;
-    $self->match_data($match_data);
-    $request->_set_route_params($match_data);
-
-    return $match_data;
-}
-
-# Does the route match the request
-sub match {
-    my ($self, $request) = @_;
-
-    my $method = lc($request->method);
-    my $path   = $request->path_info;
-    my %params;
-
-    Dancer::Logger::core("trying to match `$path' "
-          . "against /"
-          . $self->regexp
-          . "/");
-
-    my @values = $path =~ $self->regexp;
-
-    # the regex comments are how we know if we captured
-    # a splat or a megasplat
-    if( my @splat_or_megasplat
-            = $self->regexp =~ /\(\?#((?:mega)?splat)\)/g ) {
-        for ( @values ) {
-            $_ = [ split '/' => $_ ] if ( shift @splat_or_megasplat ) =~ /megasplat/;
-        }
-    }
-
-    Dancer::Logger::core("  --> got ".
-        map { defined $_ ? $_ : 'undef' } @values)
-        if @values;
-
-    # if some named captures found, return captures
-    # no warnings is for perl < 5.10
-    if (my %captures =
-        do { no warnings; %+ }
-      )
-    {
-        Dancer::Logger::core(
-            "  --> captures are: " . join(", ", keys(%captures)))
-          if keys %captures;
-        return $self->save_match_data($request, {captures => \%captures});
-    }
-
-    return unless @values;
-
-    # save the route pattern that matched
-    # TODO : as soon as we have proper Dancer::Internal, we should remove
-    # that, it's just a quick hack for plugins to access the matching
-    # pattern.
-    # NOTE: YOU SHOULD NOT USE THAT, OR IF YOU DO, YOU MUST KNOW
-    # IT WILL MOVE VERY SOON
-    $request->{_route_pattern} = $self->regexp;
-
-    # named tokens
-    my @tokens = @{$self->params};
-
-    Dancer::Logger::core("  --> named tokens are: @tokens") if @tokens;
-    if (@tokens) {
-        for (my $i = 0; $i < @tokens; $i++) {
-            $params{$tokens[$i]} = $values[$i];
-        }
-        return $self->save_match_data($request, \%params);
-    }
-
-    elsif ($self->should_capture) {
-        return $self->save_match_data($request, {splat => \@values});
-    }
-
-    return $self->save_match_data($request, {});
-}
-
+# TODO this should be moved in an upper layer
+ 
 sub run {
     my ($self, $request, $response) = @_;
 
     my $content  = $self->execute();
-#    my $response = Dancer::SharedData->response;
 
     if ( $response && $response->is_forwarded ) {
         my $new_req =
@@ -309,39 +335,4 @@ sub run {
     );
 }
 
-sub find_next_matching_route {
-    my ($self, $request) = @_;
-    my $next = $self->next;
-    return unless $next;
 
-    return $next if $next->match($request);
-    return $next->find_next_matching_route($request);
-}
-
-sub execute {
-    my ($self) = @_;
-
-    if (Dancer::Config::setting('warnings')) {
-        my $warning;
-        local $SIG{__WARN__} = sub { $warning = $_[0] };
-        my $content = $self->code->();
-        if ($warning) {
-            return Dancer::Error->new(
-                code    => 500,
-                message => "Warning caught during route execution: $warning",
-            )->render;
-        }
-        return $content;
-    }
-    else {
-        return $self->code->();
-    }
-}
-
-sub equals {
-    my ($self, $route) = @_;
-    return $self->regexp eq $route->regexp;
-}
-
-
-1;
---
+++ t/route.t
@@ -0,0 +1,57 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+use Dancer::Core::Route;
+
+my @tests = (
+    [ ['get', '/', sub { 11 }], 
+      '/', 
+      [ {}, 11] 
+    ],
+
+    [ ['get', '/hello/:name', sub { 22 }], 
+      '/hello/sukria', 
+      [ {name => 'sukria'}, 22] 
+    ],
+
+    [ ['get', '/', sub { 33 }, '/forum'], 
+      '/forum', 
+      [ {splat => [1]}, 33] 
+    ],
+    [ ['get', '/', sub { 33 }, '/forum'], 
+      '/forum/', 
+      [ {splat => [1]}, 33] 
+    ],
+
+);
+
+plan tests => scalar(@tests) * 4;
+
+for my $t (@tests) {
+    my ($route, $path, $expected) = @$t;
+
+    # successful request
+    my $r = Dancer::Core::Route->new(
+        method => $route->[0],
+        regexp => $route->[1],
+        code   => $route->[2],
+        prefix => $route->[3],
+    );
+
+    note "regexp built: ".$r->regexp;
+    isa_ok $r, 'Dancer::Core::Route';
+
+    my $m = $r->match($route->[0] => $path);
+    is_deeply $m, $expected->[0],
+        "got expected data for '$path'";
+    is $r->execute, $expected->[1],
+        "got expected result for '$path'";
+
+    # failing request
+    $m = $r->match(get => '/something_that_doesnt_exist');
+    is $m, undef, "dont match failing request";
+}
+
+done_testing;
