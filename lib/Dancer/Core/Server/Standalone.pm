2bb29cbaee828e4f2797b6375d2e42c054f044a9
Alexis Sukrieh <sukria@sukria.net>
2011-08-17 20:38:01 +0200

    Server Standalone

---
+++ lib/Dancer/Core/Server/Standalone.pm
@@ -0,0 +1,29 @@
+package Dancer::Core::Server::Standalone;
+
+use Moo;
+extends 'Dancer::Core::Server';
+
+use HTTP::Server::Simple::PSGI;
+
+has backend => (
+    is => 'rw',
+    isa => sub { Dancer::Moo::Types::ObjectOf('HTTP::Server::Simple::PSGI' => @_) },
+);
+
+sub BUILD {
+    my ($self) = @_;
+
+    $self->backend(HTTP::Server::Simple::PSGI->new($self->port));
+    $self->backend->host($self->host);
+    $self->backend->app($self->psgi_app);
+}
+
+sub start {
+    my $self = shift;
+
+    $self->is_daemon
+        ? $self->backend->background() 
+        : $self->backend->run();
+}
+
+1;
