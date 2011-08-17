3c5b47b5c207b4753baee662f62014130304a453
Alexis Sukrieh <sukria@sukria.net>
2011-08-17 20:38:17 +0200

    base class for Core::Server

---
+++ lib/Dancer/Core/Server.pm
@@ -0,0 +1,69 @@
+package Dancer::Core::Server;
+use Carp 'croak';
+use Moo;
+use Dancer::Moo::Types;
+
+use Dancer::Core::App;
+use Dancer::Core::Response;
+use Dancer::Core::Request;
+use Dancer::Core::Context;
+
+has app => (
+    is => 'ro',
+    required => 1,
+    isa => sub { Dancer::Moo::Types::ObjectOf('Dancer::Core::App' => @_) },
+);
+
+has host => (
+    is => 'ro',
+    isa => sub { Dancer::Moo::Types::Str(@_) },
+    default => sub { '0.0.0.0' },
+);
+
+has port => (
+    is => 'ro',
+    isa => sub { Dancer::Moo::Types::Num(@_) },
+    default => sub { 5000 },
+);
+
+has is_daemon => (
+    is => 'ro',
+    isa => sub { Dancer::Moo::Types::Bool(@_) },
+    default => sub { 0 },
+);
+
+sub backend { croak "backend attribute must be implemented" }
+
+sub handle_request {
+    my ($self, $request) = @_;
+    my $app = $self->app;
+    
+    my $route = $app->find_route_for_request($request);
+    return $app->response_not_found() if ! defined $route; # 404
+
+    my $context = Dancer::Core::Context->new(request => $request);
+    $app->running_context($context);
+
+    my $content;
+    eval { $content = $route->execute($context) };
+    return $app->response_internal_error($@) if $@; # 500
+
+    my $response = Dancer::Core::Response->new(
+        content => $content,
+        %{ $context->response_attributes },
+    );
+    
+    $app->running_context(undef);
+    return $response->to_psgi;
+}
+
+sub psgi_app {
+    my ($self) = @_;
+    sub {
+        my ($env) = @_;
+        my $request = Dancer::Core::Request->new(env => $env);
+        $self->handle_request($request);
+    };
+}
+
+1;
