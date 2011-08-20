ca192bf173b2fcc99e76021ddc5b077a96afcc53
Alexis Sukrieh <sukria@sukria.net>
2011-08-20 12:27:49 +0200

    use the dispatcher in Core::Role::Server, remove useless code accordingly

--- lib/Dancer/Core/Role/Server.pm
+++ lib/Dancer/Core/Role/Server.pm
@@ -5,6 +5,7 @@ use Carp 'croak';
 use Dancer::Moo::Types;
 
 use Dancer::Core::App;
+use Dancer::Core::Dispatcher;
 use Dancer::Core::Response;
 use Dancer::Core::Request;
 use Dancer::Core::Context;
@@ -38,91 +39,26 @@ has is_daemon => (
     default => sub { 0 },
 );
 
-sub handle_request {
-    my ($self, $env) = @_;
+has dispatcher => (
+    is => 'rw',
+    isa => sub { ObjectOf('Dancer::Core::Dispatcher', @_) },
+    lazy => 1,
+    builder => '_build_dispatcher',
+);
 
-    foreach my $app (@{$self->apps}) {
-        
-        # initialize a context for the current request
-        my $context = Dancer::Core::Context->new(env => $env);
-        $app->context($context);
-
-        my $route = $app->find_route_for_request($context->request);
-        next if not defined $route; # might be in the next app
-        
-        # run the before filters
-        $app->execute_hooks('before', $context);
-
-        # NOTE : here, Dancer 1 used to support request alterations by the before filters, I'm
-        # not sure this is a good idea since we have forward for that now...
-        # since this really makes the code complicated, I'd like not to support
-        # it anymore, we'll see later if that's a good idea or not.
-
-        my $content;
-        my $response;
-
-        # try to execute the route until we find one that did not pass
-        while (1) {
-            eval { $content = $route->execute($context) };
-            return $self->response_internal_error($@) if $@; # 500
-
-            # build a response with the return value of the route
-            # and the response context
-            $response = Dancer::Core::Response->new(
-                content => $content,
-                %{$context->response},
-            );
-            last unless $response->has_passed;
-
-            # the route handler passed, play again with the next one...
-            my $next = $route->next;
-            return $self->response_not_found("last route passed") unless defined $next;
-
-            # of course, we purge the response 'has_passed' flag in the context
-            # (but not the rest of it, because we want all the context to
-            # persist among all route handlers that pass.
-            $context->response->{has_passed} = 0;
-            $route = $next;
-        }
-        
-        $app->execute_hooks('after', $response);
-        $app->context(undef);
-        return $response->to_psgi;
-    }
-
-    return $self->response_not_found($env->{REQUEST_URI}) ; # 404
+sub _build_dispatcher {
+    my ($self) = @_;
+    my $d = Dancer::Core::Dispatcher->new();
+    $d->apps( $self->apps );
+    return $d;
 }
 
 sub psgi_app {
     my ($self) = @_;
     sub {
         my ($env) = @_;
-        $self->handle_request($env);
+        $self->dispatcher->dispatch($env);
     };
-}
-
-sub response_internal_error {
-    my ($self, $error) = @_;
-    
-    my $r = Dancer::Core::Response->new(
-        status => 500,
-        content => "Internal Server Error\n\n$error\n",
-        content_type => 'text/plain',
-    );
-
-    return $r->to_psgi;
-}
-
-sub response_not_found {
-    my ($self, $request) = @_;
-    
-    my $r = Dancer::Core::Response->new(
-        status => 404,
-        content => "404 Not Found\n\n$request\n",
-        content_type => 'text/plain',
-    );
-
-    return $r->to_psgi;
 }
 
 1;
