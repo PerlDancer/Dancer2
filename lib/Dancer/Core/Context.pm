e6cbbf38999fd20048de1953c748ef36a23d8798
Alexis Sukrieh <sukria@sukria.net>
2011-08-28 18:42:47 +0200

    add a cookies attr in Dancer::Core::Context (lazy)

--- lib/Dancer/Core/Context.pm
+++ lib/Dancer/Core/Context.pm
@@ -1,9 +1,11 @@
 package Dancer::Core::Context;
 use Moo;
 use Dancer::Moo::Types;
+use URI::Escape;
 
 use Dancer::Core::Request;
 use Dancer::Core::Response;
+use Dancer::Core::Cookie;
 
 # the PSGI-env to use for building the request to process
 # this is the only mandatory argument to a context
@@ -41,8 +43,34 @@ has response => (
     default => sub { Dancer::Core::Response->new },
 );
 
-sub response_is_halted {
-    return $_[0]->response->is_halted;
+has cookies => (
+    is => 'rw',
+    isa => sub { HashRef(@_) },
+    lazy => 1,
+    builder => '_build_cookies',
+);
+
+sub _build_cookies {
+    my ($self) = @_;
+
+    my $env_str = $self->env->{COOKIE} || $self->env->{HTTP_COOKIE};
+    return {} unless defined $env_str;
+
+    my $cookies = {};
+    foreach my $cookie ( split( /[,;]\s/, $env_str ) ) {
+        # here, we don't want more than the 2 first elements
+        # a cookie string can contains something like:
+        # cookie_name="foo=bar"
+        # we want `cookie_name' as the value and `foo=bar' as the value
+        my( $name,$value ) = split(/\s*=\s*/, $cookie, 2);
+        my @values;
+        if ( $value ne '' ) {
+            @values = map { uri_unescape($_) } split( /[&;]/, $value );
+        }
+        $cookies->{$name} =
+          Dancer::Core::Cookie->new( name => $name, value => \@values );
+    }
+    return $cookies;
 }
 
 1;
--- t/context.t
+++ t/context.t
@@ -15,6 +15,7 @@ my $env = {
     SERVER_PORT       => 5000,
     SERVER_PROTOCOL   => 'HTTP/1.1',
     REMOTE_ADDR       => '127.0.0.1',
+    HTTP_COOKIE       => 'dancer.session=1234; fbs_102="access_token=xxxxxxxxxx%7Cffffff"',
     X_FORWARDED_FOR => '127.0.0.2',
     REMOTE_HOST       => 'localhost',
     HTTP_USER_AGENT        => 'Mozilla',
@@ -25,5 +26,10 @@ my $c = Dancer::Core::Context->new(env => $env);
 
 isa_ok $c->request, 'Dancer::Core::Request';
 is $c->request->method, 'GET';
+
+isa_ok $c->cookies->{'dancer.session'}, 'Dancer::Core::Cookie';
+is $c->cookies->{'dancer.session'}->value, 1234;
+is $c->cookies->{'dancer.session'}->name, 'dancer.session';
+is $c->cookies->{'dancer.session'}->secure, 0;
 
 done_testing;
