6257d9c4f329e261d86bcbf43b12309d16b782f7
ambs <ambs@cpan.org>
2011-09-08 16:17:08 +0100

    Start to make things available for testing purposes

---
+++ lib/Dancer/Serializer/JSON.pm
@@ -0,0 +1,51 @@
+package Dancer::Serializer::JSON;
+
+use Moo;
+
+with 'Dancer::Core::Role::Serializer';
+
+
+# helpers
+
+sub from_json {
+    my $s = Dancer::Serializer::JSON->new;
+    $s->deserialize(@_);
+}
+
+sub to_json {
+    my $s = Dancer::Serializer::JSON->new;
+    $s->serialize(@_);
+}
+
+# class definition
+
+sub loaded { Dancer::ModuleLoader->Load('JSON') }
+
+sub serialize {
+    my ($self, $entity, $options) = @_;
+
+    # Why doesn't $self->config have this?
+    my $config = $self->config;
+
+    if ( $config->{allow_blessed} && !defined $options->{allow_blessed} ) {
+        $options->{allow_blessed} = $config->{allow_blessed};
+    }
+    if ( $config->{convert_blessed} ) {
+        $options->{convert_blessed} = $config->{convert_blessed};
+    }
+
+    if (setting('environment') eq 'development' and not defined $options->{pretty}) {
+        $options->{pretty} = 1;
+    }
+
+    JSON::to_json( $entity, $options );
+}
+
+sub deserialize {
+    my ($self, $entity, $options) = @_;
+    JSON::from_json( $entity, $options );
+}
+
+sub content_type {'application/json'}
+
+1;
