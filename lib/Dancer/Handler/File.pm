5157f8abb2451edc0737ab8e2145fbb470477150
Alexis Sukrieh <sukria@sukria.net>
2011-09-25 16:34:32 +0200

    better code for Handler::File (stolen from Plack::App::File)

--- lib/Dancer/Handler/File.pm
+++ lib/Dancer/Handler/File.pm
@@ -1,14 +1,22 @@
 package Dancer::Handler::File;
 use Carp 'croak';
 use Moo;
+use HTTP::Date;
 use Dancer::FileUtils 'path', 'read_file_content';
 use Dancer::Core::MIME;
 use Dancer::Moo::Types;
+
+with 'Dancer::Handler::Role::StandardResponses';
 
 has mime => (
     is => 'ro',
     isa => sub { ObjectOf('Dancer::Core::MIME', @_) },
     default => sub { Dancer::Core::MIME->new },
+);
+
+has encoding => (
+    is => 'ro',
+    default => sub { 'utf-8' },
 );
 
 has public_dir => (
@@ -31,17 +39,37 @@ sub code {
         my $ctx  = shift;
         my $path = $ctx->request->path_info;
 
+        if ($path =~ /\0/) {
+            return $self->response_400($ctx);
+        }
+
         my @tokens = split '/', $path;
+        if (grep $_ eq '..', @tokens) {
+            return $self->response_403($ctx);
+        }
+
         my $file_path = path($self->public_dir, @tokens);
 
-        if (! -r $file_path || ! -f $file_path) {
+        if (! -f $file_path) {
             $ctx->response->has_passed(1);
             return;
         }
 
+        if (! -r $file_path) {
+            return $self->response_403($ctx);
+        }
+
         my $content = read_file_content($file_path);
-        $ctx->response->push_header('Content-Type', $self->mime->for_file($file_path));
-        $ctx->response->push_header('Content-Length', length($content));
+        my $content_type = $self->mime->for_file($file_path) || 'text/plain';
+
+        if ($content_type =~ m!^text/!) {
+            $content_type .= "; charset=" . ($self->encoding || "utf-8");
+        }
+
+        my @stat = stat $file_path;
+        $ctx->response->push_header('Content-Type', $content_type);
+        $ctx->response->push_header('Content-Length', $stat[7]);
+        $ctx->response->push_header('Last-Modified', HTTP::Date::time2str($stat[9]));
 
         return ($ctx->request->method eq 'GET') ? $content : '';
     };
---
+++ lib/Dancer/Handler/Role/StandardResponses.pm
@@ -0,0 +1,25 @@
+package Dancer::Handler::Role::StandardResponses;
+use Moo::Role;
+
+sub response {
+    my ($self, $ctx, $code, $message) = @_;
+    $ctx->response->status($code);
+    $ctx->response->header('Content-Type', 'text/plain');
+    return $message;
+}
+
+sub response_400 {
+    my ($self, $ctx) = @_;
+    $self->response($ctx, 400, 'Bad Request');
+}
+
+sub response_404 {
+    my ($self, $ctx) = @_;
+    $self->response($ctx, 404, 'Not Found');
+}
+
+sub response_403 {
+    my ($self, $ctx) = @_;
+    $self->response($ctx, 403, 'Unauthorized');
+}
+1;
