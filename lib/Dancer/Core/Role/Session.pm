ef1fd0223cc83f616dda83e0dd8a11fa502f1b7b
Alexis Sukrieh <sukria@sukria.net>
2011-11-01 17:25:48 +0100

    fixed and better lib/Dancer/Core/Role/Session.pm

--- lib/Dancer/Core/Role/Session.pm
+++ lib/Dancer/Core/Role/Session.pm
@@ -3,11 +3,12 @@ use Dancer::Moo::Types;
 use Dancer::FileUtils qw'path';
 use File::Spec;
 
-
 use Moo::Role;
 with 'Dancer::Core::Role::Engine';
 
 sub type { 'Session' }
+
+sub supported_hooks { }
 
 # args: ($class, $id)
 # receives a session id and should return a session object if found, or undef
@@ -32,10 +33,36 @@ sub reset { return }
 has id => (
     is => 'rw',
     isa => sub { Str(@_) },
-    default => sub { _build_id() },
+    lazy => 1,
+    builder => '_build_id',
 );
 
-has session_name => (
+# we try to make the best random number
+# with native Perl 5 code.
+# to rebuild a session id, an attacker should know:
+# - the running PID of the server
+# - the current timestamp of the time it was built
+# - the path of the installation directory
+# - guess the correct number between 0 and 1000000000
+# - should be able to reproduce that 3 times
+sub _build_id {
+    my $session_id = "";
+    foreach my $seed (rand(1000), rand(1000), rand(1000)) {
+        my $c = 0;
+        $c += ord($_) for (split //, File::Spec->rel2abs(File::Spec->curdir));
+        my $current = int($seed * 1000000000) + time + $$ + $c;
+        $session_id .= $current;
+    }
+    return $session_id;
+}
+
+has data => (
+    is => 'rw',
+    isa => sub { HashRef(@_) },
+    default => sub { {} },
+);
+
+has name => (
     is => 'rw',
     isa => sub { Str(@_) },
     default => sub { 'dancer.session' },
@@ -58,40 +85,31 @@ has session_expires => (
     isa => sub { Str(@_) },
 );
 
-
 # Methods below this this line should not be overloaded.
 
-# we try to make the best random number
-# with native Perl 5 code.
-# to rebuild a session id, an attacker should know:
-# - the running PID of the server
-# - the current timestamp of the time it was built
-# - the path of the installation directory
-# - guess the correct number between 0 and 1000000000
-# - should be able to reproduce that 3 times
-sub _build_id {
-    my $session_id = "";
-    foreach my $seed (rand(1000), rand(1000), rand(1000)) {
-        my $c = 0;
-        $c += ord($_) for (split //, File::Spec->rel2abs(File::Spec->curdir));
-        my $current = int($seed * 1000000000) + time + $$ + $c;
-        $session_id .= $current;
-    }
-    return $session_id;
+sub write { 
+    my ($self, $key, $value) = @_;
+    $self->data->{$key} = $value;
+    $self->flush;
+    return $value;
 }
 
-sub read_session_id {
-    my ($self) = @_;
-
-    my $name = $self->session_name;
-    my $c = $self->context->cookies->{$name};
-    return (defined $c) ? $c->value : undef;
+sub read {
+    my ($self, $key) = @_;
+    $self->data->{$key};
 }
 
-sub write_session_id {
+sub delete { 
+    my ($self, $key) = @_;
+    delete $self->data->{$key};
+    $self->flush;
+}
+
+
+sub cookie {
     my ($self, $id) = @_;
 
-    my $name = $self->session_name;
+    my $name = $self->name;
     my %cookie = (
         name      => $name,
         value     => $self->id,
@@ -103,8 +121,7 @@ sub write_session_id {
         $cookie{expires} = $expires;
     }
 
-    $self->context->cookies->{$name} = Dancer::Core::Cookie->new(%cookie);
+    Dancer::Core::Cookie->new(%cookie);
 }
-
 
 1;
