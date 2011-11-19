568d8419b258f2f0f439c17fd8f9e75c8928f61c
Alexis Sukrieh <sukria@sukria.net>
2011-11-19 16:00:45 +0100

    fix register in DSL Role

--- lib/Dancer/Core/Role/DSL.pm
+++ lib/Dancer/Core/Role/DSL.pm
@@ -3,8 +3,6 @@ use Moo::Role;
 use Dancer::Moo::Types;
 use Carp 'croak';
 
-requires 'dsl_keywords';
-
 has app => (is => 'ro', required => 1);
 
 has keywords => (
@@ -16,19 +14,18 @@ has keywords => (
 
 sub _build_dsl_keywords {
     my ($self) = @_;
-    $self->dsl_keywords;
+    $self->can('dsl_keywords')
+        ? $self->dsl_keywords
+        : [ ] ;
 }
 
 sub register {
-    my ($self, $keyword, $code, $is_global) = @_;
+    my ($self, $keyword, $is_global) = @_;
     
     grep { /^$keyword$/ } @{$self->keywords} 
         and croak "Keyword '$keyword' is not available.";
 
-    push @{$self->keywords}, { 
-        name => $keyword,
-        code =>  $code,
-        is_global => $is_global };
+    push @{$self->keywords}, [ $keyword, $is_global ];
 }
 
 sub export_symbols_to {
@@ -38,6 +35,7 @@ sub export_symbols_to {
 
     foreach my $export (keys %{ $exports }) {
         no strict 'refs';
+        no warnings 'redefine';
         *{"${caller}::${export}"} = $exports->{$export};
     }
 
@@ -46,30 +44,32 @@ sub export_symbols_to {
 
 # private
 
+sub _compile_keyword {
+    my ($self, $keyword, $is_global) = @_;
+    
+    my $compiled_code = sub {
+        core_debug("[".$self->app->name."] -> $keyword(".join(', ', @_).")");
+        $self->$keyword(@_);
+    };
+
+    if (! $is_global) {
+        my $code = $compiled_code;
+        $compiled_code = sub {
+            croak "Function '$keyword' must be called from a route handler"
+                unless defined $self->app->context;
+            $code->(@_);
+        };
+    }
+
+    return $compiled_code;
+}
+
 sub _construct_export_map {
     my ($self) = @_;
     my %map;
     foreach my $keyword (@{ $self->keywords }) {
         my ($keyword, $is_global) = @{$keyword};
-    
-        # compile the keyword...
-
-        my $compiled_code = sub {
-            core_debug("[".$self->app->name."] -> $keyword(".join(', ', @_).")");
-            $self->$keyword(@_);
-        };
-
-        if (! $is_global) {
-            my $code = $compiled_code;
-            $compiled_code = sub {
-                croak "Function '$keyword' must be called from a route handler"
-                    unless defined $self->app->context;
-                $code->(@_);
-            };
-        }
-
-        # store it to the export map
-        $map{$keyword} = $compiled_code;
+        $map{$keyword} = $self->_compile_keyword($keyword, $is_global);
     }
     return \%map;
 }
