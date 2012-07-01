6eddcaa0240bc68f6265de4bdcd6200be99eeeae
Alexis Sukrieh <sukria@sukria.net>
2012-07-01 13:02:51 +0200

    error_template can be a scalar ref (lib/Dancer/Core/Error.pm)

--- lib/Dancer/Core/Error.pm
+++ lib/Dancer/Core/Error.pm
@@ -42,7 +42,7 @@ has title => (
 
 has error_template => (
     is => 'ro',
-    isa => sub { ReadableFilePath(@_) },
+    isa => sub { ref($_[0]) eq 'SCALAR' || ReadableFilePath(@_) },
     lazy => 1,
     builder => '_build_error_template',
 );
