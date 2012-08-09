4a18bfd67fe2c8607eeb2d2cf2abddb5bf525d62
Matt Phillips <mphillips@oanda.com>
2012-08-09 09:38:35 -0400

    Convert Dancer/Moo/Types to Dancer/Core/Types
    This commit is an initial pass at converting Dancer::Moo::Types to
    Dancer::Core::Types. This replaces the homebrewed Moo type system to instead
    leverage MooX::Types::MooseLike along with Dancer2s custom types.

---
+++ lib/Dancer/Core/Types.pm
@@ -0,0 +1,108 @@
+# ABSTRACT: TODO
+
+package Dancer::Core::Types;
+
+use strict;
+use warnings;
+use Carp 'croak';
+use Scalar::Util 'blessed', 'looks_like_number';
+use MooX::Types::MooseLike::Base qw/:all/;
+use MooX::Types::MooseLike::Numeric qw/:all/;
+
+use base 'Exporter';
+our @EXPORT;
+our @EXPORT_OK;
+
+my $single_part = qr/
+    [A-Za-z]              # must start with letter
+    (?: [A-Za-z0-9_]+ )? # can continue with letters, numbers or underscore
+/x;
+
+my $namespace = qr/
+    ^
+    $single_part                    # first part
+    (?: (?: \:\: $single_part )+ )? # optional part starting with double colon
+    $
+/x;
+
+# TODO fix ObjectOf / ConsumerOf
+my $definitions = [
+    {
+        name => 'ObjectOf',
+        test => sub { 1 },
+        message => sub { 'fail' }
+    },
+    {
+        name => 'ConsumerOf',
+        test => sub { 1 },
+        message => sub { 'fail' }
+    },
+    {
+        name => 'ReadableFilePath',
+        test => sub { -e $_[0] && -r $_[0] },
+        message => sub { "The value `$_[0]' does not pass the type constraint for type `ReadableFilePath'" }
+    },
+    {
+        name => 'WritableFilePath',
+        test => sub { -e $_[0] && -w $_[0] },
+        message => sub { "The value `$_[0]' does not pass the type constraint for type `WritableFilePath'" }
+    },
+    # Dancer-specific types
+    {
+        name => 'DancerPrefix',
+        test => sub {
+            return 1 if !defined $_[0];
+            # a prefix must start with the char '/'
+            # index is much faster than =~ /^\//
+            ref(\$_[0]) eq 'SCALAR' && (index($_[0], '/') == 0);
+        },
+        message => sub { "The value `$_[0]' does not pass the type constraint for type `DancerPrefix'" }
+    },
+    {
+        name => 'DancerAppName',
+        test => sub {
+            # TODO need a real check of valid app names
+            return 1 if !defined $_[0];
+            ref(\$_[0]) eq 'SCALAR' && $_[0] =~ $namespace
+        },
+        message => sub { "The value `$_[0]' does not pass the type constraint for type `DancerAppName'" }
+    },
+    {
+        name => 'DancerMethod',
+        test => sub {
+            return 1 if !defined $_[0];
+            grep { /^$_[0]$/ } qw(get head post put delete options)
+        },
+        message => sub { "The value `$_[0]' does not pass the type constraint for type `DancerMethod'" }
+    },
+    {
+        name => 'DancerHTTPMethod',
+        test => sub {
+            return 1 if !defined $_[0];
+            grep { /^$_[0]$/ } qw(GET HEAD POST PUT DELETE OPTIONS)
+        },
+        message => sub { "The value `$_[0]' does not pass the type constraint for type `DancerHTTPMethod'" }
+    },
+];
+
+# generate abbreviated class types for core dancer objects
+for my $type (qw/App Request Response Context Runner Dispatcher MIME/) {
+    push @$definitions, {
+        name => $type,
+        test => sub {
+            return $_[0]
+              && blessed( $_[0] )
+              && ref( $_[0] ) eq 'Dancer::Core::' . $type;
+        },
+        message => sub { "The value `$_[0]' does not pass the constraint check." }
+    };
+}
+
+MooX::Types::MooseLike::register_types($definitions, __PACKAGE__);
+
+@EXPORT = (@MooX::Types::MooseLike::Base::EXPORT_OK, @EXPORT_OK, qw(ConsumerOf ObjectOf));
+
+# Export everything by default.
+
+1;
+
