52b1a3fcc5672776008ecbe8eaca0c3d87192c33
Mateu X Hunter <hunter@missoula.org>
2012-10-11 16:42:43 -0600

    correct and refactor some types

--- lib/Dancer/Core/Types.pm
+++ lib/Dancer/Core/Types.pm
@@ -27,50 +27,54 @@ my $namespace = qr/
 
 my $definitions = [
     {
-        name => 'ReadableFilePath',
-        test => sub { -e $_[0] && -r $_[0] },
-        message => sub { "The value `$_[0]' does not pass the type constraint for type `ReadableFilePath'" }
+        name    => 'ReadableFilePath',
+        test    => sub { -e $_[0] && -r $_[0] },
+        message => sub { return exception_message($_[0], 'ReadableFilePath') }
     },
     {
-        name => 'WritableFilePath',
-        test => sub { -e $_[0] && -w $_[0] },
-        message => sub { "The value `$_[0]' does not pass the type constraint for type `WritableFilePath'" }
+        name    => 'WritableFilePath',
+        test    => sub { -e $_[0] && -w $_[0] },
+        message => sub { return exception_message($_[0], 'WritableFilePath') }
     },
     # Dancer-specific types
     {
-        name => 'DancerPrefix',
-        test => sub {
-            return 1 if !defined $_[0];
+        name       => 'DancerPrefix',
+        subtype_of => 'Str',
+        from       => 'MooX::Types::MooseLike::Base',
+        test       => sub {
             # a prefix must start with the char '/'
             # index is much faster than =~ /^\//
-            ref(\$_[0]) eq 'SCALAR' && (index($_[0], '/') == 0);
+            index($_[0], '/') == 0;
         },
-        message => sub { "The value `$_[0]' does not pass the type constraint for type `DancerPrefix'" }
+        message    => sub { return exception_message($_[0], 'DancerPrefix') }
     },
     {
-        name => 'DancerAppName',
-        test => sub {
+        name       => 'DancerAppName',
+        subtype_of => 'Str',
+        from       => 'MooX::Types::MooseLike::Base',
+        test       => sub {
             # TODO need a real check of valid app names
-            return 1 if !defined $_[0];
-            ref(\$_[0]) eq 'SCALAR' && $_[0] =~ $namespace
+            $_[0] =~ $namespace;
         },
-        message => sub { "The value `$_[0]' does not pass the type constraint for type `DancerAppName'" }
+        message    => sub { return exception_message($_[0], 'DancerAppName') }
     },
     {
-        name => 'DancerMethod',
-        test => sub {
-            return 1 if !defined $_[0];
+        name       => 'DancerMethod',
+        subtype_of => 'Str',
+        from       => 'MooX::Types::MooseLike::Base',
+        test       => sub {
             grep { /^$_[0]$/ } qw(get head post put delete options patch)
         },
-        message => sub { "The value `$_[0]' does not pass the type constraint for type `DancerMethod'" }
+        message    => sub { return exception_message($_[0], 'DancerMethod') }
     },
     {
-        name => 'DancerHTTPMethod',
+        name       => 'DancerHTTPMethod',
+        subtype_of => 'Str',
+        from       => 'MooX::Types::MooseLike::Base',
         test => sub {
-            return 1 if !defined $_[0];
             grep { /^$_[0]$/ } qw(GET HEAD POST PUT DELETE OPTIONS PATCH)
         },
-        message => sub { "The value `$_[0]' does not pass the type constraint for type `DancerHTTPMethod'" }
+        message => sub { return exception_message($_[0], 'DancerHTTPMethod') }
     },
 ];
 
@@ -91,6 +95,12 @@ MooX::Types::MooseLike::register_types($definitions, __PACKAGE__);
 
 # Export everything by default.
 @EXPORT = (@MooX::Types::MooseLike::Base::EXPORT_OK, @EXPORT_OK);
+
+sub exception_message {
+    my ($attribute_value, $type) = @_;
+    $attribute_value = defined $attribute_value ? $attribute_value : 'undef';
+    return "The value `${attribute_value}' does not pass the type constraint for type `${type}'";
+} 
 
 1;
 
