52aa5b3f0e88c209bce1ee3e8f541dc8dc3ea9f1
smashz <mestre.smash@gmail.com>
2013-07-31 17:46:04 +0100

    fix typos

--- lib/Dancer2/Plugin.pm
+++ lib/Dancer2/Plugin.pm
@@ -55,7 +55,7 @@ in order to set some options.
 The option C<is_global> (boolean) is used to declare a global/non-global keyword 
 (by default all keywords are global). A non-global keyword must be called from
 within a route handler (eg: C<session> or C<param>) whereas a global one can be
-called frome everywhere (eg: C<dancer_version> or C<setting>).
+called from everywhere (eg: C<dancer_version> or C<setting>).
 
     register my_symbol_to_export => sub {
         # ... some code
