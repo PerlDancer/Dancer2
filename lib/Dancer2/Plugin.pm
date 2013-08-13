ca1fe5ae74e0eda3fb5f9099d7f638c533a7d356
Sawyer X <xsawyerx@cpan.org>
2013-08-13 11:54:05 +0300

    remove the syntax import tag

--- lib/Dancer2/Plugin.pm
+++ lib/Dancer2/Plugin.pm
@@ -33,8 +33,8 @@ when the symbol is called.
 
 The coderef receives as its first argument the Dancer2::Core::DSL object.
 
-Instead of importing C<use Dancer ':syntax'>, plugins B<must> use the DSL
-object to access application components and work with them directly.
+Plugins B<must> use the DSL object to access application components and work
+with them directly.
 
     sub {
         my $dsl = shift;
