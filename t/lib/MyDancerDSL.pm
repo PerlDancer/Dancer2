5e52541eb76cd732d4acbafdaa8da67222e60bc3
mickey <mickey75@gmail.com>
2013-08-16 00:02:30 +0200

    changed dsl_keywords to hashref, managing a key/value pair structure in AoA doesn't make much sense, and bug in checking existing keys (fixed in a23012b47) made it possible to re-register the same keyword even with a different (is_global) value which may lead to unexpected behavior. (t/plugin_syntax.t is still broken)

--- t/lib/MyDancerDSL.pm
+++ t/lib/MyDancerDSL.pm
@@ -12,12 +12,12 @@ around dsl_keywords => sub {
     my $orig     = shift;
     my $keywords = $orig->(@_);
 
-    push @$keywords, [ gateau => 0 ],    # cookie
-      [ moteur => 1 ],                   # engine
-      [ stop   => 0 ],                   # halt
-      [ prend  => 1 ],                   # post
-      [ envoie => 1 ],                   # post
-      [ entete => 0 ];                   #header
+    $keywords->{gateau} = 0; # cookie
+    $keywords->{moteur} = 1; # engine
+    $keywords->{stop}   = 0; # halt
+    $keywords->{prend}  = 1; # post
+    $keywords->{envoie} = 1; # post
+    $keywords->{entete} = 0; #header
 
     return $keywords;
 };
