36328b93ccc19d5acd487c92d93f9371c21410f6
Russell Jenkins <veryrusty@gmail.com>
2014-04-23 13:44:17 +1000

    Note appname import in Dancer2.pm
    Also note that config setting at import time is current not implemented.

--- lib/Dancer2.pm
+++ lib/Dancer2.pm
@@ -193,8 +193,9 @@ Import gets called when you use Dancer2. You can specify import options giving
 you control over the keywords that will be imported into your webapp and other
 things:
 
-    use Dancer2 ( foo => 'bar' ); # sets option foo to bar
     use Dancer2 '!quux'; # Don't import DSL keyword quux
+    use Dancer2 appname => 'MyAwesomeApp'; # Add routes and hooks to MyAwesomeApp
+    use Dancer2 ( foo => 'bar' ); # sets option foo to bar (currently not implemented)
 
 =head1 AUTHORS
 
