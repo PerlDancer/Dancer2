13683a1f72c9cd0cb55120b0e324e2c8e0d6330c
Davs <kovdavid@gmail.com>
2014-09-19 10:36:35 +0200

    Remove public_dir from Handler::File's default config

--- lib/Dancer2/Core/App.pm
+++ lib/Dancer2/Core/App.pm
@@ -510,10 +510,7 @@ sub _build_default_config {
                             || path( $self->location, 'public' ) ),
         route_handlers => [
             [
-                File => {
-                    public_dir => $ENV{DANCER_PUBLIC} ||
-                                  path( $self->location, 'public' )
-                }
+                File => 1
             ],
             [
                 AutoPage => 1
