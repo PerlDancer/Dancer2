2448c3f7cc20dc6bcaaaf7fb7e11f572b7da6e86
Paul Cochrane <paul@liekut.de>
2014-09-21 23:23:03 +0200

    Moving POD after __END__ (Plugin.pm)

--- lib/Dancer2/Plugin.pm
+++ lib/Dancer2/Plugin.pm
@@ -1,66 +1,9 @@
-package Dancer2::Plugin;
-
 # ABSTRACT: Extending Dancer2's DSL with plugins
-
-=head1 DESCRIPTION
-
-You can extend Dancer2 by writing your own plugin. A plugin is a module that
-exports a bunch of symbols to the current namespace (the caller will see all
-the symbols defined via C<register>).
-
-Note that you have to C<use> the plugin wherever you want to use its symbols.
-For instance, if you have Webapp::App1 and Webapp::App2, both loaded from your
-main application, they both need to C<use FooPlugin> if they want to use the
-symbols exported by C<FooPlugin>.
-
-For a more gentle introduction to Dancer2 plugins, see L<Dancer2::Plugins>.
-
-=cut
+package Dancer2::Plugin;
 
 use Moo::Role;
 use Carp 'croak', 'carp';
 use Dancer2::Core::DSL;
-
-=method register
-
-    register 'my_keyword' => sub { ... } => \%options;
-
-Allows the plugin to define a keyword that will be exported to the caller's
-namespace.
-
-The first argument is the symbol name, the second one the coderef to execute
-when the symbol is called.
-
-The coderef receives as its first argument the Dancer2::Core::DSL object.
-
-Plugins B<must> use the DSL object to access application components and work
-with them directly.
-
-    sub {
-        my $dsl = shift;
-        my @args = @_;
-
-        my $app     = $dsl->app;
-        my $request = $app->request;
-
-        if ( $app->session->read('logged_in') ) {
-            ...
-        }
-    };
-
-As an optional third argument, it's possible to give a hash ref to C<register>
-in order to set some options.
-
-The option C<is_global> (boolean) is used to declare a global/non-global keyword
-(by default all keywords are global). A non-global keyword must be called from
-within a route handler (eg: C<session> or C<param>) whereas a global one can be
-called from everywhere (eg: C<dancer_version> or C<setting>).
-
-    register my_symbol_to_export => sub {
-        # ... some code
-    }, { is_global => 1} ;
-
-=cut
 
 # singleton for storing all keywords,
 # their code and the plugin they come from
@@ -99,55 +42,12 @@ sub register {
       [ $keyword, $code, $options->{is_global} ];
 }
 
-=method on_plugin_import
-
-Allows the plugin to take action each time it is imported.
-It is prototyped to take a single code block argument, which will be called
-with the DSL object of the package importing it.
-
-For example, here is a way to install a hook in the importing app:
-
-    on_plugin_import {
-        my $dsl = shift;
-        $dsl->app->add_hook(
-            Dancer2::Core::Hook->new(
-                name => 'before',
-                code => sub { ... },
-            )
-        );
-    };
-
-=cut
-
 sub on_plugin_import(&) {
     my $code   = shift;
     my $plugin = caller;
     $_on_import->{$plugin} ||= [];
     push @{ $_on_import->{$plugin} }, $code;
 }
-
-=method register_plugin
-
-A Dancer2 plugin must end with this statement. This lets the plugin register all
-the symbols defined with C<register> as exported symbols:
-
-    register_plugin;
-
-Register_plugin returns 1 on success and undef if it fails.
-
-=head3 Deprecation note
-
-Earlier version of Dancer2 needed the keyword <for_version> to indicate for
-which version of Dancer the plugin was written, e.g.
-
-    register_plugin for_versions => [ 2 ];
-
-Today, plugins for Dancer2 are only expected to work for Dancer2 and the
-C<for_versions> keyword is ignored. If you try to load a plugin for Dancer2
-that does not meet the requirements of a Dancer2 plugin, you will get an error
-message.
-
-=cut
 
 sub register_plugin {
     my $plugin = caller;
@@ -207,45 +107,7 @@ sub register_plugin {
     # The plugin is ready now.
 }
 
-=method plugin_args
-
-Simple method to retrieve the parameters or arguments passed to a
-plugin-defined keyword. Although not relevant for Dancer 1 only, or
-Dancer 2 only, plugins, it is useful for universal plugins.
-
-  register foo => sub {
-     my ($dsl, @args) = plugin_args(@_);
-     ...
-  }
-
-Note that Dancer 1 will return undef as the DSL object.
-
-=cut
-
 sub plugin_args {@_}
-
-=method plugin_setting
-
-If C<plugin_setting> is called inside a plugin, the appropriate configuration
-will be returned. The C<plugin_name> should be the name of the package, or,
-if the plugin name is under the B<Dancer2::Plugin::> namespace (which is
-recommended), the remaining part of the plugin name.
-
-Configuration for plugin should be structured like this in the config.yml of
-the application:
-
-  plugins:
-    plugin_name:
-      key: value
-
-Enclose the remaining part in quotes if it contains ::, e.g.
-for B<Dancer2::Plugin::Foo::Bar>, use:
-
-  plugins:
-    "Foo::Bar":
-      key: value
-
-=cut
 
 sub plugin_setting {
     my $plugin = caller;
@@ -254,16 +116,6 @@ sub plugin_setting {
     my $app = $dsl->dancer_app;
     return $app->config->{'plugins'}->{$plugin_name} ||= {};
 }
-
-=method register_hook
-
-Allows a plugin to declare a list of supported hooks. Any hook declared like so
-can be executed by the plugin with C<execute_hook>.
-
-    register_hook 'foo';
-    register_hook 'foo', 'bar', 'baz';
-
-=cut
 
 sub register_hook {
     my $caller = caller;
@@ -299,21 +151,6 @@ sub register_hook {
         *{"${caller}::hook_aliases"}    = sub {$current_aliases};
     }
 }
-
-=method execute_hook
-
-Allows a plugin to execute the hooks attached at the given position
-
-    execute_hook 'some_hook';
-
-Arguments can be passed which will be received by handlers attached to that
-hook:
-
-    execute_hook 'some_hook', $some_args, ... ;
-
-The hook must have been registered by the plugin first, with C<register_hook>.
-
-=cut
 
 sub execute_hook {
     my $position = shift;
@@ -401,6 +238,154 @@ sub _get_dsl {
 }
 
 1;
+
+__END__
+
+=head1 DESCRIPTION
+
+You can extend Dancer2 by writing your own plugin. A plugin is a module that
+exports a bunch of symbols to the current namespace (the caller will see all
+the symbols defined via C<register>).
+
+Note that you have to C<use> the plugin wherever you want to use its symbols.
+For instance, if you have Webapp::App1 and Webapp::App2, both loaded from your
+main application, they both need to C<use FooPlugin> if they want to use the
+symbols exported by C<FooPlugin>.
+
+For a more gentle introduction to Dancer2 plugins, see L<Dancer2::Plugins>.
+
+=method register
+
+    register 'my_keyword' => sub { ... } => \%options;
+
+Allows the plugin to define a keyword that will be exported to the caller's
+namespace.
+
+The first argument is the symbol name, the second one the coderef to execute
+when the symbol is called.
+
+The coderef receives as its first argument the Dancer2::Core::DSL object.
+
+Plugins B<must> use the DSL object to access application components and work
+with them directly.
+
+    sub {
+        my $dsl = shift;
+        my @args = @_;
+
+        my $app     = $dsl->app;
+        my $request = $app->request;
+
+        if ( $app->session->read('logged_in') ) {
+            ...
+        }
+    };
+
+As an optional third argument, it's possible to give a hash ref to C<register>
+in order to set some options.
+
+The option C<is_global> (boolean) is used to declare a global/non-global keyword
+(by default all keywords are global). A non-global keyword must be called from
+within a route handler (eg: C<session> or C<param>) whereas a global one can be
+called from everywhere (eg: C<dancer_version> or C<setting>).
+
+    register my_symbol_to_export => sub {
+        # ... some code
+    }, { is_global => 1} ;
+
+=method on_plugin_import
+
+Allows the plugin to take action each time it is imported.
+It is prototyped to take a single code block argument, which will be called
+with the DSL object of the package importing it.
+
+For example, here is a way to install a hook in the importing app:
+
+    on_plugin_import {
+        my $dsl = shift;
+        $dsl->app->add_hook(
+            Dancer2::Core::Hook->new(
+                name => 'before',
+                code => sub { ... },
+            )
+        );
+    };
+
+=method register_plugin
+
+A Dancer2 plugin must end with this statement. This lets the plugin register all
+the symbols defined with C<register> as exported symbols:
+
+    register_plugin;
+
+Register_plugin returns 1 on success and undef if it fails.
+
+=head3 Deprecation note
+
+Earlier version of Dancer2 needed the keyword <for_version> to indicate for
+which version of Dancer the plugin was written, e.g.
+
+    register_plugin for_versions => [ 2 ];
+
+Today, plugins for Dancer2 are only expected to work for Dancer2 and the
+C<for_versions> keyword is ignored. If you try to load a plugin for Dancer2
+that does not meet the requirements of a Dancer2 plugin, you will get an error
+message.
+
+=method plugin_args
+
+Simple method to retrieve the parameters or arguments passed to a
+plugin-defined keyword. Although not relevant for Dancer 1 only, or
+Dancer 2 only, plugins, it is useful for universal plugins.
+
+  register foo => sub {
+     my ($dsl, @args) = plugin_args(@_);
+     ...
+  }
+
+Note that Dancer 1 will return undef as the DSL object.
+
+=method plugin_setting
+
+If C<plugin_setting> is called inside a plugin, the appropriate configuration
+will be returned. The C<plugin_name> should be the name of the package, or,
+if the plugin name is under the B<Dancer2::Plugin::> namespace (which is
+recommended), the remaining part of the plugin name.
+
+Configuration for plugin should be structured like this in the config.yml of
+the application:
+
+  plugins:
+    plugin_name:
+      key: value
+
+Enclose the remaining part in quotes if it contains ::, e.g.
+for B<Dancer2::Plugin::Foo::Bar>, use:
+
+  plugins:
+    "Foo::Bar":
+      key: value
+
+=method register_hook
+
+Allows a plugin to declare a list of supported hooks. Any hook declared like so
+can be executed by the plugin with C<execute_hook>.
+
+    register_hook 'foo';
+    register_hook 'foo', 'bar', 'baz';
+
+=method execute_hook
+
+Allows a plugin to execute the hooks attached at the given position
+
+    execute_hook 'some_hook';
+
+Arguments can be passed which will be received by handlers attached to that
+hook:
+
+    execute_hook 'some_hook', $some_args, ... ;
+
+The hook must have been registered by the plugin first, with C<register_hook>.
 
 =head1 EXAMPLE PLUGIN
 
