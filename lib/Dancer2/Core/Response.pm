4093cd8bcd12e3a2586e7119bb267217b7b47217
Paul Cochrane <paul@liekut.de>
2014-09-22 12:24:09 +0200

    Moving POD after __END__ (Core/Response.pm)

--- lib/Dancer2/Core/Response.pm
+++ lib/Dancer2/Core/Response.pm
@@ -28,20 +28,7 @@ has has_passed => (
     default => sub {0},
 );
 
-=method pass
-
-Set has_passed to true.
-
-=cut
-
 sub pass { shift->has_passed(1) }
-
-
-=method serializer()
-
-Returns the optional serializer object used to deserialize request parameters
-
-=cut
 
 has serializer => (
     is        => 'ro',
@@ -49,25 +36,11 @@ has serializer => (
     predicate => 1,
 );
 
-
-=attr is_encoded
-
-Flag to tell if the content has already been encoded.
-
-=cut
-
 has is_encoded => (
     is      => 'rw',
     isa     => Bool,
     default => sub {0},
 );
-
-
-=attr is_halted
-
-Flag to tell whether or not the response should continue to be processed.
-
-=cut
 
 has is_halted => (
     is      => 'rw',
@@ -75,20 +48,7 @@ has is_halted => (
     default => sub {0},
 );
 
-=method halt
-
-Shortcut to halt the current response by setting the is_halted flag.
-
-=cut
-
 sub halt { shift->is_halted(1) }
-
-
-=attr status
-
-The HTTP status for the response.
-
-=cut
 
 has status => (
     is      => 'rw',
@@ -105,17 +65,6 @@ has status => (
         $value;
     },
 );
-
-
-=attr content
-
-The content for the response, stored as a string.  If a reference is passed, the
-response will try coerce it to a string via double quote interpolation.
-
-Whenever the content changes, it recalculates and updates the Content-Length header,
-unless the response has_passed.
-
-=cut
 
 has content => (
     is      => 'rw',
@@ -143,30 +92,11 @@ before content => sub {
     }
 };
 
-=attr default_content_type
-
-Default mime type to use for the response Content-Type header
-if nothing was specified
-
-=cut
-
 has default_content_type => (
     is      => 'rw',
     isa     => Str,
     default => sub {'text/html'},
 );
-
-=method encode_content
-
-Encodes the stored content according to the stored L<content_type>.  If the content_type
-is a text format C<^text>, then no encoding will take place.
-
-Interally, it uses the L<is_encoded> flag to make sure that content is not encoded twice.
-
-If it encodes the content, then it will return the encoded content.  In all other
-cases it returns C<false>.
-
-=cut
 
 sub encode_content {
     my ($self) = @_;
@@ -188,13 +118,6 @@ sub encode_content {
     return $content;
 }
 
-
-=method to_psgi
-
-Converts the response object to a PSGI array.
-
-=cut
-
 sub to_psgi {
     my ($self) = @_;
     # It is possible to have no content and/or no content type set
@@ -203,13 +126,6 @@ sub to_psgi {
     $self->content('') if ! defined $self->content;
     return [ $self->status, $self->headers_to_array, [ $self->content ], ];
 }
-
-
-=method content_type($type)
-
-A little sugar for setting or accessing the content_type of the response, via the headers.
-
-=cut
 
 # sugar for accessing the content_type header, with mimetype care
 sub content_type {
@@ -240,15 +156,6 @@ sub is_forwarded {
     $self->_forward;
 }
 
-
-=method redirect ($destination, $status)
-
-Sets a header in this response to give a redirect to $destination, and sets the
-status to $status.  If $status is omitted, or false, then it defaults to a status of
-302.
-
-=cut
-
 sub redirect {
     my ( $self, $destination, $status ) = @_;
     $self->status( $status || 302 );
@@ -256,16 +163,6 @@ sub redirect {
     # we want to stringify the $destination object (URI object)
     $self->header( 'Location' => "$destination" );
 }
-
-
-=method error( @args )
-
-    $response->error( message => "oops" );
-
-Creates a L<Dancer2::Core::Error> object with the given I<@args> and I<throw()>
-it against the response object. Returns the error object.
-
-=cut
 
 sub error {
     my $self = shift;
@@ -279,16 +176,6 @@ sub error {
     return $error;
 }
 
-
-=method serialize( $content )
-
-    $response->serialize( $content );
-
-Serialize and return $content with the response's serializer.
-set content-type accordingly.
-
-=cut
-
 sub serialize {
     my ($self, $content) = @_;
     return unless $self->has_serializer;
@@ -301,3 +188,82 @@ sub serialize {
 }
 
 1;
+
+__END__
+
+=method pass
+
+Set has_passed to true.
+
+=method serializer()
+
+Returns the optional serializer object used to deserialize request parameters
+
+=attr is_encoded
+
+Flag to tell if the content has already been encoded.
+
+=attr is_halted
+
+Flag to tell whether or not the response should continue to be processed.
+
+=method halt
+
+Shortcut to halt the current response by setting the is_halted flag.
+
+=attr status
+
+The HTTP status for the response.
+
+=attr content
+
+The content for the response, stored as a string.  If a reference is passed, the
+response will try coerce it to a string via double quote interpolation.
+
+Whenever the content changes, it recalculates and updates the Content-Length header,
+unless the response has_passed.
+
+=attr default_content_type
+
+Default mime type to use for the response Content-Type header
+if nothing was specified
+
+=method encode_content
+
+Encodes the stored content according to the stored L<content_type>.  If the content_type
+is a text format C<^text>, then no encoding will take place.
+
+Interally, it uses the L<is_encoded> flag to make sure that content is not encoded twice.
+
+If it encodes the content, then it will return the encoded content.  In all other
+cases it returns C<false>.
+
+=method to_psgi
+
+Converts the response object to a PSGI array.
+
+=method content_type($type)
+
+A little sugar for setting or accessing the content_type of the response, via the headers.
+
+=method redirect ($destination, $status)
+
+Sets a header in this response to give a redirect to $destination, and sets the
+status to $status.  If $status is omitted, or false, then it defaults to a status of
+302.
+
+=method error( @args )
+
+    $response->error( message => "oops" );
+
+Creates a L<Dancer2::Core::Error> object with the given I<@args> and I<throw()>
+it against the response object. Returns the error object.
+
+=method serialize( $content )
+
+    $response->serialize( $content );
+
+Serialize and return $content with the response's serializer.
+set content-type accordingly.
+
+=cut
