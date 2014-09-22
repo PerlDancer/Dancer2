1f8aa922ac06a6dfadcea4e572d0b3fdb7e3eb8c
Paul Cochrane <paul@liekut.de>
2014-09-22 09:02:14 +0200

    Moving POD after __END__ (Core/Cookie.pm)

--- lib/Dancer2/Core/Cookie.pm
+++ lib/Dancer2/Core/Cookie.pm
@@ -8,34 +8,6 @@ use Dancer2::Core::Time;
 use Carp 'croak';
 use overload '""' => \&_get_value;
 
-=head1 SYNOPSIS
-
-    use Dancer2::Core::Cookie;
-
-    my $cookie = Dancer2::Core::Cookie->new(
-        name => $cookie_name, value => $cookie_value
-    );
-
-    my $value = $cookie->value;
-
-    print "$cookie"; # objects stringify to their value.
-
-=head1 DESCRIPTION
-
-Dancer2::Core::Cookie provides a HTTP cookie object to work with cookies.
-
-=method my $cookie=Dancer2::Core::Cookie->new(%opts);
-
-Create a new Dancer2::Core::Cookie object.
-
-You can set any attribute described in the I<ATTRIBUTES> section above.
-
-=method my $header=$cookie->to_header();
-
-Creates a proper HTTP cookie header from the content.
-
-=cut
-
 sub to_header {
     my $self   = shift;
     my $header = '';
@@ -52,20 +24,6 @@ sub to_header {
 
     return join '; ', @headers;
 }
-
-
-=attr value
-
-The cookie's value.
-
-(Note that cookie objects use overloading to stringify to their value, so if
-you say e.g. return "Hi, $cookie", you'll get the cookie's value there.)
-
-In list context, returns a list of potentially multiple values; in scalar
-context, returns just the first value.  (So, if you expect a cookie to have
-multiple values, use list context.)
-
-=cut
 
 has value => (
     is       => 'rw',
@@ -92,19 +50,92 @@ around value => sub {
 # attribute accessor won't be available at that point.
 sub _get_value { shift->value }
 
-
-=attr name
-
-The cookie's name.
-
-=cut
-
 has name => (
     is       => 'rw',
     isa      => Str,
     required => 1,
 );
 
+has expires => (
+    is       => 'rw',
+    isa      => Str,
+    required => 0,
+    coerce   => sub {
+        Dancer2::Core::Time->new( expression => $_[0] )->gmt_string;
+    },
+);
+
+has domain => (
+    is       => 'rw',
+    isa      => Str,
+    required => 0,
+);
+
+has path => (
+    is        => 'rw',
+    isa       => Str,
+    default   => sub {'/'},
+    predicate => 1,
+);
+
+has secure => (
+    is       => 'rw',
+    isa      => Bool,
+    required => 0,
+    default  => sub {0},
+);
+
+has http_only => (
+    is       => 'rw',
+    isa      => Bool,
+    required => 0,
+    default  => sub {0},
+);
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use Dancer2::Core::Cookie;
+
+    my $cookie = Dancer2::Core::Cookie->new(
+        name => $cookie_name, value => $cookie_value
+    );
+
+    my $value = $cookie->value;
+
+    print "$cookie"; # objects stringify to their value.
+
+=head1 DESCRIPTION
+
+Dancer2::Core::Cookie provides a HTTP cookie object to work with cookies.
+
+=method my $cookie=Dancer2::Core::Cookie->new(%opts);
+
+Create a new Dancer2::Core::Cookie object.
+
+You can set any attribute described in the I<ATTRIBUTES> section above.
+
+=method my $header=$cookie->to_header();
+
+Creates a proper HTTP cookie header from the content.
+
+=attr value
+
+The cookie's value.
+
+(Note that cookie objects use overloading to stringify to their value, so if
+you say e.g. return "Hi, $cookie", you'll get the cookie's value there.)
+
+In list context, returns a list of potentially multiple values; in scalar
+context, returns just the first value.  (So, if you expect a cookie to have
+multiple values, use list context.)
+
+=attr name
+
+The cookie's name.
 
 =attr expires
 
@@ -116,56 +147,18 @@ It also supports a human readable offset from the current time such as "2 hours"
 See the documentation of L<Dancer2::Core::Time> for details of all supported
 formats.
 
-=cut
-
-has expires => (
-    is       => 'rw',
-    isa      => Str,
-    required => 0,
-    coerce   => sub {
-        Dancer2::Core::Time->new( expression => $_[0] )->gmt_string;
-    },
-);
-
-
 =attr domain
 
 The cookie's domain.
-
-=cut
-
-has domain => (
-    is       => 'rw',
-    isa      => Str,
-    required => 0,
-);
 
 =attr path
 
 The cookie's path.
 
-=cut
-
-has path => (
-    is        => 'rw',
-    isa       => Str,
-    default   => sub {'/'},
-    predicate => 1,
-);
-
 =attr secure
 
 If true, it instructs the client to only serve the cookie over secure
 connections such as https.
-
-=cut
-
-has secure => (
-    is       => 'rw',
-    isa      => Bool,
-    required => 0,
-    default  => sub {0},
-);
 
 =attr http_only
 
@@ -177,12 +170,3 @@ If your cookie is meant to be used by some JavaScript code, set this
 attribute to 0.
 
 =cut
-
-has http_only => (
-    is       => 'rw',
-    isa      => Bool,
-    required => 0,
-    default  => sub {0},
-);
-
-1;
