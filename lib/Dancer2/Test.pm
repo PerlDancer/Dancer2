1925f62fba7b0d0f0987680e13b0d5e1f99b5dc8
Sawyer X <xsawyerx@cpan.org>
2014-10-24 01:13:19 +0200

    cleanup

--- lib/Dancer2/Test.pm
+++ lib/Dancer2/Test.pm
@@ -613,16 +613,20 @@ __END__
 
 =head1 SYNOPSIS
 
-  use Plack::Test;
-  use HTTP::Request::Common; # not included in Dancer2
+    use Test::More;
+    use Plack::Test;
+    use HTTP::Request::Common; # install separately
 
-  use YourDancerApp;
-  my $app = YourDancerApp->psgi_app;
-  my $test = Plack::Test->create($app);
+    use YourDancerApp;
 
-  my $res = $test->request(GET "/");  
-  is $res->code, 200, "GET / is found";
-  like $res->content, qr/hello, world/, "content looks good for /";
+    my $app  = YourDancerApp->to_app;
+    my $test = Plack::Test->create($app);
+
+    my $res = $test->request( GET '/' );
+    is( $res->code, 200, '[GET /] Request successful' );
+    like( $res->content, qr/hello, world/, '[GET /] Correct content';
+
+    done_testing;
 
 =head1 DESCRIPTION
 
