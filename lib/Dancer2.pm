4a60fd3092377f30dc81ff292212c977db9ee1f8
Sawyer X <xsawyerx@cpan.org>
2014-12-04 11:34:03 +0100

    bump version

--- lib/Dancer2.pm
+++ lib/Dancer2.pm
@@ -15,7 +15,7 @@ our $AUTHORITY = 'SUKRIA';
 # set version in dist.ini now
 # but we still need a basic version for
 # the tests
-$Dancer2::VERSION ||= '0.155003'; # 2.150.3
+$Dancer2::VERSION ||= '0.155004'; # 2.150.4
 
 our $runner;
 
