34977c05e21c98149b589b87d5856904860c2302
Russell Jenkins <veryrusty@gmail.com>
2014-12-14 14:16:18 +1100

    Remove modification of content_type attribute.
    The docs for the content_type attribute in D2::Core::Role::Serializer
    state this the the content type *after* serialization. However we were
    setting this attribute with (almost) every public method in
    Serializer::Mutable, which, well, sucks.
    
    Instead, we give the `content_type` attribute the default
    (applicaiton/json), and rename the reader to `default_content_type`
    (which seems apt), and add a method `content_type` that checks the
    request headers to return the approriate content type we sill serialize
    to.
    
    However; there is scope here for some confusion having both an attribute
    and method with the same name for anyone working on the code in the
    future..

--- lib/Dancer2/Serializer/Mutable.pm
+++ lib/Dancer2/Serializer/Mutable.pm
@@ -6,7 +6,18 @@ use Carp 'croak';
 use Encode;
 with 'Dancer2::Core::Role::Serializer';
 
-has '+content_type' => (default => 1);    # to satify 'required'
+has '+content_type' => (
+    default => 'application/json',      # default content type
+    reader  => 'default_content_type',  # accessor for the default
+);
+
+# The content type *after* serialization.
+sub content_type {
+    my $self = shift;
+    return $self->_get_content_type(qw<accept content_type>)
+        || $self->default_content_type;
+}
+
 
 my $formats = {
     'text/x-yaml'        => 'YAML',
@@ -34,11 +45,7 @@ my $serializer = {
 sub support_content_type {
     my ( $self, $ct ) = @_;
 
-    # FIXME: are we getting full content type?
-
     if ( $ct && grep +( $_ eq $ct ), keys %{$formats} ) {
-        $self->set_content_type($ct);
-
         return 1;
     }
 
@@ -49,10 +56,10 @@ sub serialize {
     my ( $self, $entity ) = @_;
 
     # Look for valid format in the headers
-    my $format = $self->_get_content_type(qw<accept content_type>);
+    my $ct = $self->content_type;
 
     # Match format with a serializer and return
-    $format and return $serializer->{$format}{'to'}->(
+    exists $formats->{$ct} and return $serializer->{$formats->{$ct}}{'to'}->(
         $self, $entity
     );
 
@@ -64,44 +71,26 @@ sub deserialize {
     my ( $self, $content ) = @_;
 
     # Look for valid format in the headers
-    my $format = $self->_get_content_type(qw<content_type accept>);
-
-    $format and return $serializer->{$format}{'from'}->( $self, $content );
+    my $ct = $self->_get_content_type(qw<content_type accept>) || $self->default_content_type;
+    exists $formats->{$ct} and return $serializer->{$formats->{$ct}}{'from'}->(
+        $self, $content
+    );
 
     return $content;
 }
 
+# returns the content type from the appropriate headers
 sub _get_content_type {
     my ( $self, @methods ) = @_;
     $self->has_request or return;
 
     # Search for the first HTTP header variable which
     # specifies supported content.
-    foreach my $method ( @methods ) {
+    for my $method ( @methods ) {
         if ( my $value = $self->request->header($method) ) {
-            if ( exists $formats->{$value} ) {
-                $self->set_content_type($value);
-                return $formats->{$value};
-            }
-
-            # If header found but type not compatible, return undef.
-            $self->set_content_type('text/plain');
-            return;
+            exists $formats->{$value} and return $value;
         }
     }
-
-    # If no header found, try default.
-    my $content_type = 'application/json';    # TODO make this a config option
-
-    # Try to serialize.
-    if (exists $formats->{$content_type}) {
-        $self->set_content_type($content_type);
-        return $formats->{$content_type};
-    }
-
-    # Fallback: plaintext without serialization.
-    $self->set_content_type('text/plain');
-    return;
 }
 
 1;
@@ -185,9 +174,12 @@ Deserialize the provided serialized data to a data structure.  The type of
 serialization format depends on the request's content-type. For now, it can
 be one of YAML, Dumper, JSON.
 
+=head2 default_content_type
+
+Returns the default content type for the serializer to use if no other type
+can be determined from the request headers.
+
 =head2 content_type
 
-Returns the content-type that was used during the last C<serialize> /
-C<deserialize> call. B<WARNING> : you must call C<serialize> / C<deserialize>
-before calling C<content_type>. Otherwise the return value will be any value
-remaing from the last request.
+Returns the content-type of the data structure AFTER being serialized.
+
