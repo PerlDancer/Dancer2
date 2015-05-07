7d3580b612f4508c21ad2161e276a2b4ebc0f7f3
Sawyer X <xsawyerx@cpan.org>
2015-05-07 11:44:11 +0200

    Refactor scheme method

--- lib/Dancer2/Core/Request.pm
+++ lib/Dancer2/Core/Request.pm
@@ -155,8 +155,8 @@ sub is_behind_proxy { $_[0]->{'is_behind_proxy'} || 0 }
 sub host {
     my ($self) = @_;
 
-    if ( $self->is_behind_proxy and exists $self->env->{HTTP_X_FORWARDED_HOST} ) {
-        my @hosts = split /\s*,\s*/, $self->env->{HTTP_X_FORWARDED_HOST}, 2;
+    if ( $self->is_behind_proxy and exists $self->env->{'HTTP_X_FORWARDED_HOST'} ) {
+        my @hosts = split /\s*,\s*/, $self->env->{'HTTP_X_FORWARDED_HOST'}, 2;
         return $hosts[0];
     } else {
         return $self->env->{'HTTP_HOST'};
@@ -178,17 +178,11 @@ sub forwarded_protocol    {
 
 sub scheme {
     my ($self) = @_;
-    my $scheme;
-    if ( $self->is_behind_proxy ) {
-        # Note the 'HTTP_' prefix the PSGI spec adds to headers.
-        $scheme =
-             $self->env->{'HTTP_X_FORWARDED_PROTOCOL'}
-          || $self->env->{'HTTP_X_FORWARDED_PROTO'}
-          || $self->env->{'HTTP_FORWARDED_PROTO'}
-          || '';
-    }
+    my $scheme = $self->is_behind_proxy
+               ? $self->forwarded_protocol
+               : '';
 
-    return $scheme || $self->env->{'psgi.url_scheme'} || '';
+    return $scheme || $self->env->{'psgi.url_scheme'};
 }
 
 sub serializer { $_[0]->{'serializer'} }
