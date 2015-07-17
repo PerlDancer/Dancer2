e634ac7b303d759e1d8f14d6eebe6ef45b338256
Russell Jenkins <veryrusty@gmail.com>
2015-07-17 23:14:34 +1000

    Dynamically generate TT2's INCLUDE_PATH
    By using a dynamic generator for TT2's INCLUDE_PATH, changes to the
    "views" setting will be honoured when rendering templates after the
    TT2 object is instantiated.
    (Otherwise the INCLUDE_PATH is static.)
    
    Some deployment options do not work with relative paths for `views`
    (FastCGI is one). To help minimise changes involved, this also adds
    a new configuration option; `include_path`  (note the lower case).
    It is prepended to the list of paths when building the INCLUDE_PATH
    generator, allowing users to add a known path to the template search
    path based on deployment.  Any existing INCLUDE_PATH (upper case)
    specified in configuration will take precedence.

--- lib/Dancer2/Template/TemplateToolkit.pm
+++ lib/Dancer2/Template/TemplateToolkit.pm
@@ -6,6 +6,7 @@ use Moo;
 use Carp qw/croak/;
 use Dancer2::Core::Types;
 use Dancer2::FileUtils qw'path';
+use Scalar::Util qw();
 use Template;
 
 with 'Dancer2::Core::Role::Template';
@@ -29,7 +30,12 @@ sub _build_engine {
     $tt_config{'END_TAG'} = $stop_tag
       if defined $stop_tag && $stop_tag ne '%]';
 
-    $tt_config{'INCLUDE_PATH'} ||= $self->views;
+    Scalar::Util::weaken( my $ttt = $self );
+    my $include_path = $self->config->{include_path};
+    $tt_config{'INCLUDE_PATH'} ||= [
+        ( defined $include_path ? $include_path : () ),
+        sub { [ $ttt->views ] },
+    ];
 
     return Template->new(%tt_config);
 }
