3cc5b22f2a6ce460989adbdb89277209bb1a7f72
Bas Bloemsaat <bas@bloemsaat.com>
2015-09-08 09:02:22 +0200

    fix 5.8 + test

--- lib/Dancer2/Core/Request.pm
+++ lib/Dancer2/Core/Request.pm
@@ -342,7 +342,7 @@ sub _decode {
     }
 
     if ( ref($h) eq 'HASH' ) {
-        return { map _decode($_) // undef, %$h };
+        return { map {my $t = _decode($_); $t} (%$h) };
     }
 
     if ( ref($h) eq 'ARRAY' ) {
