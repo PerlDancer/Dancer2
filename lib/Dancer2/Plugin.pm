b7851c1b15dc3e4859f94b7eeac11af5a682df50
Sawyer X <xsawyerx@cpan.org>
2016-08-09 00:45:42 +0200

    Remove redefined warning

--- lib/Dancer2/Plugin.pm
+++ lib/Dancer2/Plugin.pm
@@ -487,6 +487,7 @@ sub register_plugin {
 
                 {
                     no strict 'refs';
+                    no warnings 'redefine';
                     *{"${plugin_module}::dsl"} = sub {$dsl};
                 }
 
