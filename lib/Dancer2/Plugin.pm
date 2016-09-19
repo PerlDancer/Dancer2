ab6cabafeb4852f4c3dddd2538c0d881138d8267
Sawyer X <xsawyerx@cpan.org>
2016-09-19 16:42:45 +0200

    Fix another edge case in plugin compat:
    Here's the thing. I don't know how to describe this problem. I
    never seem to be in the zone enough to fully understand it or
    explain it. However, I have these bits of gleaned information
    which I quote below.
    
        So, I think it's like this: If you have a plugin that uses
        DSL directly (which are only available on D2P1, in D2P2 you
        have to use `$self`⁠⁠), anyou didn't call `register_plugin`,
        those will not be available yet, and when you try to call them,
        it will fail that they do not exist.
    
        (Which makes sense. We create them for you, with a warning,
        if you call `⁠register_plug⁠`, which you would do for D2P1
        plugin anyway.)
    
        However, and here's the kicker, if you do this while defining
        your `appname` to a class that does not exist, you get a
        different error. The error you get is that the specified
        `appname` class cannot call the keyword you wanted.
    
        This is because there was one more place in which I didn't
        resolve GH #1226.
    
    Here, then, is the final fix. It should be to just use the found
    "app_cb_dsl" class, but that broke a test. Ugh. Anyway, crazy,
    but works. How? Not sure.

--- lib/Dancer2/Plugin.pm
+++ lib/Dancer2/Plugin.pm
@@ -522,8 +522,9 @@ sub register_plugin {
                     no strict 'refs';
                     $plugin_module->can($keyword)
                       or *{"${plugin_module}::$keyword"} = sub {
-                          my $coderef = shift()->app->name->can($keyword);
-                          $coderef->(@_);
+                        $_[0]
+                          ? do { my $cc = shift()->app->name->can($keyword); $cc->(@_) }
+                          : $app_dsl_cb->(@_);
                       };
                 }
             });
