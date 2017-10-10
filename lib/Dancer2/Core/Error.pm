5a3839dd3605f5f70b978bf65bd34375cd76495b
Nick Tonkin <nick@nicktonkin.net>
2017-10-10 09:29:05 -0400

    When show_errors is on, only skip showing the static error page in the case of a 500.

--- lib/Dancer2/Core/Error.pm
+++ lib/Dancer2/Core/Error.pm
@@ -264,9 +264,11 @@ sub _build_content {
         return $content if defined $content;
     }
 
-    # It doesn't make sense to return a static page if show_errors is on
-    if ( !$self->show_errors && (my $content = $self->static_page) ) {
-        return $content;
+    # It doesn't make sense to return a static page for a 500 if show_errors is on
+    if ( !($self->show_errors && $self->status == '500') ) {
+         if ( my $content = $self->static_page ) {
+             return $content;
+         }
     }
 
     if ($self->has_app && $self->app->config->{error_template}) {
