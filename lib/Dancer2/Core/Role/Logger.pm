80b04fe5d8c7fe449798a96c29e5d5a6b60282c1
simbabque <simbabque@cpan.org>
2019-03-19 13:26:30 +0000

    don't decode already decoded date strings
    This blew up before in certain locales, such as ru_RU.UTF-8 or ko_KR.UTF-8 or he_IL.UTF-8. I've talked it through at https://stackoverflow.com/q/55240390/1331451, where we concluded that the problem is that it's decoding things that are already decoded, and therefore dies. As Encode does not show the error message by default, this just silently fails.

--- lib/Dancer2/Core/Role/Logger.pm
+++ lib/Dancer2/Core/Role/Logger.pm
@@ -81,10 +81,7 @@ sub format_message {
     my $block_handler = sub {
         my ( $block, $type ) = @_;
         if ( $type eq 't' ) {
-            return Encode::decode(
-                $config->{'charset'} || 'UTF-8',
-                POSIX::strftime( $block, localtime(time) )
-            );
+            return POSIX::strftime( $block, localtime(time) );
         }
         elsif ( $type eq 'h' ) {
             return ( $request && $request->header($block) ) || '-';
@@ -97,19 +94,9 @@ sub format_message {
 
     my $chars_mapping = {
         a => sub { $self->app_name },
-        t => sub {
-            Encode::decode(
-                $config->{'charset'} || 'UTF-8',
-                POSIX::strftime( "%d/%b/%Y %H:%M:%S", localtime(time) )
-            );
-        },
+        t => sub { POSIX::strftime( "%d/%b/%Y %H:%M:%S", localtime(time) ) },
         T => sub { POSIX::strftime( "%Y-%m-%d %H:%M:%S", localtime(time) ) },
-        u => sub {
-            Encode::decode(
-                $config->{'charset'} || 'UTF-8',
-                POSIX::strftime( "%d/%b/%Y %H:%M:%S", gmtime(time) )
-            );
-        },
+        u => sub { POSIX::strftime( "%d/%b/%Y %H:%M:%S", gmtime(time) ) },
         U => sub { POSIX::strftime( "%Y-%m-%d %H:%M:%S", gmtime(time) ) },
         P => sub {$$},
         L => sub {$level},
