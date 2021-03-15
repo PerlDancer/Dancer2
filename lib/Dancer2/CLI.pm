d7452c4da1770b8305f92082f3a203c6c49ab138
David Precious <davidp@preshweb.co.uk>
2021-03-15 20:24:14 +0000

    More detailed error message.

--- lib/Dancer2/CLI.pm
+++ lib/Dancer2/CLI.pm
@@ -9,7 +9,21 @@ BEGIN {
         require App::Cmd::Setup;
         1; 
     } or do {
-        warn "ERROR: You need to install App::Cmd first to use this tool";
+        warn <<INSTALLAPPCMD;
+ERROR: You need to install App::Cmd first to use this tool.
+
+You can do so using your preferred module installation method, for instance;
+
+  # using cpanminus
+  cpanm App::Cmd
+  # or using CPAN.pm
+  cpan App::Cmd
+  
+For more detailed instructions, see http://www.cpan.org/modules/INSTALL.html
+
+Without App::Cmd, the `dancer2` app minting tool cannot be used, but Dancer2
+can still be used for existing apps.
+INSTALLAPPCMD
         exit;
     }
 }
