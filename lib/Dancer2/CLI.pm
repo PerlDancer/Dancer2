fa97fecfd24cf613d2583d8c60e2c351c8716641
David Precious <davidp@preshweb.co.uk>
2021-03-15 20:13:57 +0000

    script/dancer2 bail out if App::Cmd not installed
    Check if App::Cmd is installed and bail out if not.
    
    TODO: better message for users on older perls explaining that they'll
    need to install App::Cmd <= 0.331, maybe with a couple of copy & paste
    ways to install that version via cpanm and CPAN.pm etc.

--- lib/Dancer2/CLI.pm
+++ lib/Dancer2/CLI.pm
@@ -3,6 +3,17 @@ package Dancer2::CLI;
 
 use strict;
 use warnings;
+
+BEGIN {
+    eval {
+        require App::Cmd::Setup;
+        1; 
+    } or do {
+        warn "ERROR: You need to install App::Cmd first to use this tool";
+        exit;
+    }
+}
+
 use App::Cmd::Setup -app;
 
 1;
