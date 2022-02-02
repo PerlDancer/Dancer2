9d4795b4340467a86046179dbdc7e9ff237871e9
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2022-02-02 21:43:14 +0100

    Create ConfigReader role and an implementation
    Create Dancer2::Core::Role::ConfigReader and Dancer2::ConfigReader::File::Simple.
    
    Signed-off-by: Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>

---
+++ lib/Dancer2/Core/Role/ConfigReader.pm
@@ -0,0 +1,125 @@
+# ABSTRACT: Config reader role for Dancer2 core objects
+package Dancer2::Core::Role::ConfigReader;
+
+use Moo::Role;
+
+use File::Spec;
+use Config::Any;
+use Hash::Merge::Simple;
+use Carp 'croak';
+use Module::Runtime 'require_module';
+
+use Dancer2::Core::Factory;
+use Dancer2::Core;
+use Dancer2::Core::Types;
+use Dancer2::FileUtils 'path';
+
+has config_location => (
+    is      => 'ro',
+    isa     => ReadableFilePath,
+    lazy    => 1,
+    default => sub { $ENV{DANCER_CONFDIR} || $_[0]->location },
+);
+
+# The type for this attribute is Str because we don't require
+# an existing directory with configuration files for the
+# environments.  An application without environments is still
+# valid and works.
+has environments_location => (
+    is      => 'ro',
+    isa     => Str,
+    lazy    => 1,
+    default => sub {
+        $ENV{DANCER_ENVDIR}
+          || File::Spec->catdir( $_[0]->config_location, 'environments' )
+          || File::Spec->catdir( $_[0]->location,        'environments' );
+    },
+);
+
+# It is required to get environment from the caller.
+# Environment should be passed down from Dancer2::Core::App.
+has environment => (
+    is       => 'ro',
+    isa      => Str,
+    required => 1,
+);
+
+# It is required to get location from the caller.
+has location => (
+    is       => 'ro',
+    isa      => ReadableFilePath,
+    required => 1,
+);
+
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+This role is implemented by different
+config readers. A config reader creates the
+configuration for Dancer2 app.
+Config can be created by reading configuration
+files, from environment variables, by fetching
+it from a cloud service, or any other means.
+
+By default, the config loader
+which is used, is C<Dancer2::ConfigReader::File::Simple>
+but user can create his own config reader
+if he wants to replace or augment
+the default method of config creation.
+That method should implement this role.
+
+The implementing module gets the following parameters
+during creation:
+
+=attr environment
+
+The name of the environment used, e.g.
+production, development, staging.
+
+==attr location
+
+Absolute path to the directory where the server started.
+
+These arguments are passed when the object is created by
+C<Dancer2::Core::Role::Config>. How the config
+reader applies them as it needs.
+
+Provides a C<config> attribute that - when accessing
+the first time - feeds itself by finding and parsing
+configuration files.
+
+Also provides a C<setting()> method which is
+supposed to be used by externals to
+read/write config entries.
+
+=head1 ATTRIBUTES
+
+=attr location
+
+Absolute path to the directory where the server started.
+
+=attr config_location
+
+Gets the location from the configuration. Same as C<< $object->location >>.
+
+=attr environments_location
+
+Gets the directory where the environment files are stored.
+
+=attr config
+
+Returns the whole configuration.
+
+=attr environments
+
+Returns the name of the environment.
+
+=head1 METHODS
+
+=head2 read_config
+
+Load the configuration.
+Whatever source the config comes from, files, env vars, etc.
