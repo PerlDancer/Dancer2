dafd2ce5ba560c196079ebba31b376741a2aac0f
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2022-03-27 14:39:33 +0200

    Rework POD and add an example how to extend
    Signed-off-by: Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>

--- lib/Dancer2/ConfigReader/File/Simple.pm
+++ lib/Dancer2/ConfigReader/File/Simple.pm
@@ -107,12 +107,84 @@ __END__
 
 =head1 DESCRIPTION
 
-This class provides the same features to read configuration files
-as were earlier done by C<Dancer2::Core::Role::Config>.
+This class is an implementation of C<Dancer2::Core::Role::ConfigReader>.
+It reads the configuration files of C<Dancer2>.
+
+Please see C<Dancer2::Config> for more information.
 
 If you need to add additional functionality to the reading
 mechanism, you can extend this class. An example of this is
 in: B</t/lib/Dancer2/ConfigReader/File/Extended.pm>
+
+An example of this is providing the possibility to replace
+random parts of the file config with environmental variables:
+
+    package Dancer2::ConfigReader::File::Extended;
+
+    use Moo;
+    use Dancer2::Core::Types;
+
+    use Carp 'croak';
+
+    extends 'Dancer2::ConfigReader::File::Simple';
+
+    has name => (
+        is      => 'ro',
+        isa     => Str,
+        lazy    => 0,
+        default => sub {'File::Extended'},
+    );
+
+    around read_config => sub {
+        my ($orig, $self) = @_;
+        my $config = $orig->($self, @_);
+        $self->_replace_env_vars($config);
+        return $config;
+    };
+
+    # Attn. We are traversing along the original data structure all the time,
+    # using references, and changing values on the spot, not returning anything.
+    sub _replace_env_vars {
+        my ( $self, $entry ) = @_;
+        if( ref $entry ne 'HASH' && ref $entry ne 'ARRAY' ) {
+            croak 'Param entry is not HASH or ARRAY';
+        }
+        if( ref $entry eq 'HASH' ) {
+            foreach my $value (values %{ $entry }) {
+                if( (ref $value) =~ m/(HASH|ARRAY)/msx ) {
+                        $self->_replace_env_vars( $value );
+                    } elsif( (ref $value) =~ m/(CODE|REF|GLOB)/msx ) {
+                        # Pretty much anything else except SCALAR. Do nothing
+                        1;
+                    } else {
+                        if( $value ) {
+                            while( my ($k, $v) = each %ENV) {
+                                $value =~ s/ \$ [{] ENV:$k [}] /$v/gmsx;
+                            }
+                        }
+                    }
+                }
+            } else {
+                # ref $entry is 'ARRAY'
+                foreach my $value (@{ $entry }) {
+                    if( (ref $value) =~ m/(HASH|ARRAY)/msx ) {
+                            $self->_replace_env_vars( $value );
+                    } elsif( (ref $value) =~ m/(CODE|REF|GLOB)/msx ) {
+                        # Pretty much anything else except SCALAR. Do nothing
+                        1;
+                    } else {
+                        if( $value ) {
+                            while( my ($k, $v) = each %ENV) {
+                                $value =~ s/ \$ [{] ENV:$k [}] /$v/gmsx;
+                            }
+                        }
+                    }
+                }
+            }
+        return;
+    }
+
+    1;
 
 =head1 ATTRIBUTES
 
