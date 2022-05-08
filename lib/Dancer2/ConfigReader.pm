975d678280e8c0a5aaf1b589ffb34d95e169f10f
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2022-05-08 20:23:54 +0200

    Split Role::Config to ConfigReader and Role::HasConfig
    Signed-off-by: Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>

--- lib/Dancer2/ConfigReader.pm
+++ lib/Dancer2/ConfigReader.pm
@@ -1,33 +1,36 @@
-# ABSTRACT: Config role for Dancer2 core objects
-package Dancer2::Core::Role::Config;
+# ABSTRACT: Config reader for Dancer2 App
+package Dancer2::ConfigReader;
 
-use Moo::Role;
+use Moo;
 
 use File::Spec;
 use Config::Any;
 use Hash::Merge::Simple;
 use Carp 'croak';
-use Module::Runtime qw{ require_module use_module };
+use Module::Runtime qw{ use_module };
 
 use Dancer2::Core::Factory;
 use Dancer2::Core;
 use Dancer2::Core::Types;
-use Dancer2::FileUtils 'path';
+use Dancer2::ConfigUtils 'normalize_config_entry';
 
-with 'Dancer2::Core::Role::HasLocation';
+has location => (
+    is       => 'ro',
+    isa      => Str,
+    required => 1,
+);
 
 has default_config => (
     is      => 'ro',
     isa     => HashRef,
-    lazy    => 1,
-    builder => '_build_default_config',
+    required => 1,
 );
 
 has config_location => (
     is      => 'ro',
     isa     => ReadableFilePath,
     lazy    => 1,
-    default => sub { $ENV{DANCER_CONFDIR} || $_[0]->location },
+    default => sub { $_[0]->location },
 );
 
 # The type for this attribute is Str because we don't require
@@ -40,8 +43,7 @@ has environments_location => (
     lazy    => 1,
     default => sub {
         $ENV{DANCER_ENVDIR}
-          || File::Spec->catdir( $_[0]->config_location, 'environments' )
-          || File::Spec->catdir( $_[0]->location,        'environments' );
+          || File::Spec->catdir( $_[0]->config_location, 'environments' );
     },
 );
 
@@ -55,64 +57,23 @@ has config => (
 has environment => (
     is      => 'ro',
     isa     => Str,
-    lazy    => 1,
-    builder => '_build_environment',
+    required => 1,
 );
 
 has config_readers => (
     is      => 'ro',
     lazy    => 1,
+    #isa     => ArrayRef[ InstanceOf['Dancer2::Core::Role::ConfigReader'] ],
     isa     => ArrayRef,
     builder => '_build_config_readers',
 );
 
-has local_triggers => (
-    is      => 'ro',
-    isa     => HashRef,
-    default => sub { +{} },
-);
-
-has global_triggers => (
-    is      => 'ro',
-    isa     => HashRef,
-    default => sub {
-        my $triggers = {
-            traces => sub {
-                my ( $self, $traces ) = @_;
-                # Carp is already a dependency
-                $Carp::Verbose = $traces ? 1 : 0;
-            },
-        };
-
-        my $runner_config;
-        {
-            no warnings 'once';
-            $runner_config = defined $Dancer2::runner
-                           ? Dancer2->runner->config
-                           : {};
-        }
-
-        for my $global ( keys %$runner_config ) {
-            next if exists $triggers->{$global};
-            $triggers->{$global} = sub {
-                my ($self, $value) = @_;
-                Dancer2->runner->config->{$global} = $value;
-            }
-        }
-
-        return $triggers;
-    },
-);
-
-sub _build_default_config { +{} }
-
-sub _build_environment { 'development' }
-
-# The new config builder
+# The config builder
 sub _build_config {
     my ($self) = @_;
 
     my $default  = $self->default_config;
+    use Data::Dumper;
     my $config = Hash::Merge::Simple->merge(
         $default,
         map {
@@ -125,6 +86,16 @@ sub _build_config {
     return $config;
 }
 
+sub _normalize_config {
+    my ( $self, $config ) = @_;
+
+    foreach my $key ( keys %{$config} ) {
+        my $value = $config->{$key};
+        $config->{$key} = normalize_config_entry( $key, $value );
+    }
+    return $config;
+}
+
 sub _build_config_readers {
     my ($self) = @_;
 
@@ -132,106 +103,13 @@ sub _build_config_readers {
                             ? (split qr{ \s+ }msx, $ENV{'DANCER_CONFIG_READERS'})
                             : ( q{Dancer2::ConfigReader::File::Simple} );
 
+    warn "ConfigReaders to use: @config_reader_names\n" if $ENV{DANCER_CONFIG_VERBOSE};
     return [
         map use_module($_)->new(
             location    => $self->location,
             environment => $self->environment,
         ), @config_reader_names
     ];
-}
-
-sub _set_config_entries {
-    my ( $self, @args ) = @_;
-    my $no = scalar @args;
-    while (@args) {
-        $self->_set_config_entry( shift(@args), shift(@args) );
-    }
-    return $no;
-}
-
-sub _set_config_entry {
-    my ( $self, $name, $value ) = @_;
-
-    $value = $self->_normalize_config_entry( $name, $value );
-    $value = $self->_compile_config_entry( $name, $value, $self->config );
-    $self->config->{$name} = $value;
-}
-
-sub _normalize_config {
-    my ( $self, $config ) = @_;
-
-    foreach my $key ( keys %{$config} ) {
-        my $value = $config->{$key};
-        $config->{$key} = $self->_normalize_config_entry( $key, $value );
-    }
-    return $config;
-}
-
-sub _compile_config {
-    my ( $self, $config ) = @_;
-
-    foreach my $key ( keys %{$config} ) {
-        my $value = $config->{$key};
-        $config->{$key} =
-          $self->_compile_config_entry( $key, $value, $config );
-    }
-    return $config;
-}
-
-sub settings { shift->config }
-
-sub setting {
-    my $self = shift;
-    my @args = @_;
-
-    return ( scalar @args == 1 )
-      ? $self->settings->{ $args[0] }
-      : $self->_set_config_entries(@args);
-}
-
-sub has_setting {
-    my ( $self, $name ) = @_;
-    return exists $self->config->{$name};
-}
-
-# private
-
-my $_normalizers = {
-    charset => sub {
-        my ($charset) = @_;
-        return $charset if !length( $charset || '' );
-
-        require_module('Encode');
-        my $encoding = Encode::find_encoding($charset);
-        croak
-          "Charset defined in configuration is wrong : couldn't identify '$charset'"
-          unless defined $encoding;
-        my $name = $encoding->name;
-
-        # Perl makes a distinction between the usual perl utf8, and the strict
-        # utf8 charset. But we don't want to make this distinction
-        $name = 'utf-8' if $name eq 'utf-8-strict';
-        return $name;
-    },
-};
-
-sub _normalize_config_entry {
-    my ( $self, $name, $value ) = @_;
-    $value = $_normalizers->{$name}->($value)
-      if exists $_normalizers->{$name};
-    return $value;
-}
-
-sub _compile_config_entry {
-    my ( $self, $name, $value, $config ) = @_;
-
-    my $trigger = exists $self->local_triggers->{$name} ?
-                         $self->local_triggers->{$name} :
-                         $self->global_triggers->{$name};
-
-    defined $trigger or return $value;
-
-    return $trigger->( $self, $value, $config );
 }
 
 1;
@@ -339,21 +217,10 @@ Gets the directory where the environment files are stored.
 =attr config
 
 Returns the whole configuration.
+This must not be used directly.
+Instead, use this via C<Dancer2::Core::Role::HasConfig> role
+which manages configuration after it is created.
 
 =attr environment
 
 Returns the name of the environment.
-
-=head1 METHODS
-
-=head2 settings
-
-Alias for config. Equivalent to <<$object->config>>.
-
-=head2 setting
-
-Get or set an element from the configuration.
-
-=head2 has_setting
-
-Verifies that a key exists in the configuration.
