91fafd3ca09e2e68e22c20291877dec0168cc1e2
Sawyer X <xsawyerx@cpan.org>
2026-01-12 23:20:00 +0100

    GH #763: Strict config: Warn on unknown config keys with opt-out:
    This adds a default-on config option `strict_config` that checks
    whether all of the keys in the config are spelled correclty and exist
    and warns accordingly.
    
    It checks for top-level keys and known built-in engines. It does *not*
    check for any plugin or other cases that do not come with Dancer2
    because they are able to access arbitrary keys.
    
    (Of course, it does not know how to check for misspelled
    `strict_config`. :)
    
    Includes allowlists for core keys and built-in engines (logger/session/
    serializer/template) so valid engine options don’t produce noise.
    
    Add a focused regression test that exercises warnings and the opt-out.

--- lib/Dancer2/ConfigReader.pm
+++ lib/Dancer2/ConfigReader.pm
@@ -8,7 +8,7 @@ use Config::Any;
 use Hash::Merge::Simple;
 use Carp 'croak';
 use Module::Runtime qw{ use_module };
-use Ref::Util qw/ is_arrayref /;
+use Ref::Util qw/ is_arrayref is_hashref /;
 use Scalar::Util qw/ blessed /;
 
 use Dancer2::Core::Factory;
@@ -17,6 +17,106 @@ use Dancer2::Core::Types;
 use Dancer2::ConfigUtils 'normalize_config_entry';
 
 our $MAX_CONFIGS = $ENV{DANCER_MAX_CONFIGS} || 100;
+
+my %KNOWN_CORE_KEYS = map +( $_ => 1 ), qw(
+    additional_config_readers
+    appdir
+    apphandler
+    appname
+    auto_page
+    behind_proxy
+    charset
+    content_type
+    default_mime_type
+    engines
+    environment
+    error_template
+    host
+    layout
+    layout_dir
+    log
+    logger
+    no_default_middleware
+    no_server_tokens
+    plugins
+    port
+    public_dir
+    route_handlers
+    serializer
+    session
+    show_errors
+    show_stacktrace
+    startup_info
+    static_handler
+    template
+    timeout
+    traces
+    type_library
+    views
+    strict_config
+);
+
+my %KNOWN_ENGINE_CONFIG = (
+    'logger' => {
+        'base_keys' => {
+            'app_name'              => 1,
+            'auto_encoding_charset' => 1,
+            'log_format'            => 1,
+            'log_level'             => 1,
+        },
+        'engines' => {
+            'capture' => { keys => {} },
+            'console' => { keys => {} },
+            'diag'    => { keys => {} },
+            'file'    => {
+                'keys' => {
+                    'file_name' => 1,
+                    'log_dir'   => 1,
+                },
+            },
+            'note'    => { keys => {} },
+            'null'    => { keys => {} },
+        },
+    },
+    'serializer' => {
+        'engines' => {
+            'dumper' => { keys => {} },
+            'json'   => { allow_any => 1 },
+            'mutable' => {
+                keys => {
+                    mapping => 1,
+                },
+            },
+            'yaml' => { keys => {} },
+        },
+    },
+    'session' => {
+        'base_keys' => {
+            'cookie_domain'    => 1,
+            'cookie_duration'  => 1,
+            'cookie_name'      => 1,
+            'cookie_path'      => 1,
+            'cookie_same_site' => 1,
+            'is_http_only'     => 1,
+            'is_secure'        => 1,
+            'session_duration' => 1,
+        },
+        'engines' => {
+            'simple' => { keys => {} },
+            'yaml'   => {
+                'keys' => {
+                    'session_dir' => 1,
+                },
+            },
+        },
+    },
+    'template' => {
+        'engines' => {
+            'templatetoolkit' => { allow_any => 1 },
+            'tiny'            => { allow_any => 1 },
+        },
+    },
+);
 
 has location => (
     is       => 'ro',
@@ -137,6 +237,9 @@ sub _normalize_config {
         my $value = $config->{$key};
         $config->{$key} = normalize_config_entry( $key, $value );
     }
+
+    $self->_strict_config_keys($config);
+
     return $config;
 }
 
@@ -154,6 +257,86 @@ sub _build_config_readers {
             environment => $self->environment,
         ), @config_reader_names
     ];
+}
+
+sub _strict_config_keys {
+    my ( $self, $config ) = @_;
+
+    return
+        if exists $config->{'strict_config'}
+        && !$config->{'strict_config'};
+
+    my @warnings = map +(
+        $KNOWN_CORE_KEYS{$_}
+        ? ()
+        : "Unknown configuration key '$_'"
+    ), sort keys %{$config};
+
+    if ( my $engines = $config->{'engines'} ) {
+        push @warnings, $self->_warn_unknown_engine_config_keys($engines);
+    }
+
+    @warnings or return;
+
+    warn join(
+        "\n",
+        @warnings,
+        'Set strict_config => 0 to silence these warnings.'
+    ) . "\n";
+}
+
+sub _warn_unknown_engine_config_keys {
+    my ( $self, $engines ) = @_;
+    is_hashref($engines)
+        or return;
+
+    my @warnings;
+
+    for my $engine_type ( sort keys %{$engines} ) {
+        my $type_config = $engines->{$engine_type};
+        is_hashref($type_config)
+            or next;
+
+        my $known_type = $KNOWN_ENGINE_CONFIG{$engine_type}
+            or next;
+
+        my $base_keys     = $known_type->{'base_keys'} || {};
+        my $known_engines = $known_type->{'engines'}   || {};
+
+        for my $engine_name ( sort keys %{$type_config} ) {
+            my $engine_config = $type_config->{$engine_name};
+            is_hashref($engine_config)
+                or next;
+
+            my $normalized = _normalize_engine_name($engine_name);
+            defined $normalized
+                or next;
+
+            my $known_engine = $known_engines->{$normalized} or next;
+            next if $known_engine->{'allow_any'};
+
+            my %allowed = (%{$base_keys}, %{ $known_engine->{keys} || {} });
+
+            for my $key ( sort keys %{$engine_config} ) {
+                next if $allowed{$key};
+                push @warnings,
+                    "Unknown configuration key '$key' for engine '$engine_type/$engine_name'";
+            }
+        }
+    }
+
+    return @warnings;
+}
+
+sub _normalize_engine_name {
+    my ($name) = @_;
+
+    return if !defined $name;
+    return if $name =~ /::/xms;
+
+    my $normalized = lc $name;
+    $normalized =~ s/_//xmsg;
+    return $normalized;
 }
 
 1;
---
+++ t/strict_config.t
@@ -0,0 +1,119 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Dancer2::ConfigReader;
+use Dancer2::FileUtils qw/dirname path/;
+use File::Spec;
+
+{
+    package Dancer2::ConfigReader::TestWarn;
+    use Moo;
+    with 'Dancer2::Core::Role::ConfigReader';
+
+    has name => (
+        is      => 'ro',
+        default => sub { 'TestWarn' },
+    );
+
+    has config_data => (
+        is      => 'ro',
+        default => sub { {} },
+    );
+
+    sub read_config {
+        my ($self) = @_;
+        return $self->config_data;
+    }
+}
+
+my $location = File::Spec->rel2abs( path( dirname(__FILE__), 'config' ) );
+
+sub _read_config_with_warnings {
+    my ($config_data) = @_;
+
+    my $reader = Dancer2::ConfigReader::TestWarn->new(
+        environment => 'test',
+        location    => $location,
+        config_data => $config_data,
+    );
+
+    my $cfgr = Dancer2::ConfigReader->new(
+        environment    => 'test',
+        location       => $location,
+        default_config => {},
+        config_readers => [$reader],
+    );
+
+    my @warnings;
+    local $SIG{__WARN__} = sub { push @warnings, @_ };
+
+    $cfgr->config;
+
+    return join q{}, @warnings;
+}
+
+subtest 'warns on unknown keys' => sub {
+    my $warnings = _read_config_with_warnings({
+        typo => 1,
+        engines => {
+            logger => {
+                File => {
+                    log_dir   => '/tmp',
+                    log_level => 'debug',
+                    extra     => 1,
+                },
+            },
+            template => {
+                template_toolkit => {
+                    foo => 1,
+                },
+            },
+            serializer => {
+                JSON => {
+                    allow_nonref => 1,
+                    foo          => 2,
+                },
+            },
+        },
+    });
+
+    like(
+        $warnings,
+        qr/Unknown configuration key 'typo'/,
+        'warns for unknown top-level key',
+    );
+    like(
+        $warnings,
+        qr/Unknown configuration key 'extra' for engine 'logger\/File'/,
+        'warns for unknown engine key',
+    );
+    unlike(
+        $warnings,
+        qr/template_toolkit/,
+        'does not warn for template_toolkit keys',
+    );
+    unlike(
+        $warnings,
+        qr/serializer\/JSON/,
+        'does not warn for JSON serializer keys',
+    );
+};
+
+subtest 'can disable warnings' => sub {
+    my $warnings = _read_config_with_warnings({
+        strict_config => 0,
+        typo          => 1,
+        engines => {
+            logger => {
+                File => {
+                    extra => 1,
+                },
+            },
+        },
+    });
+
+    is( $warnings, q{}, 'warnings silenced' );
+};
+
+done_testing;
